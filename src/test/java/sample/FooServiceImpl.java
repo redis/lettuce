cd8b1d195c2e4d18da9a3fa862387a9b5de36194
Simone Tripodi <simone.tripodi@gmail.com>
2010-08-29 15:16:24 +0000

    started importing existing tests

---
+++ src/test/java/sample/FooServiceImpl.java
@@ -0,0 +1,47 @@
+/*
+ *    Copyright 2010 The myBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package sample;
+
+import java.util.List;
+
+/**
+ * 
+ * @version $Id$
+ */
+public class FooServiceImpl implements FooService {
+
+    private UserMapper userMapper;
+
+    public UserMapper getUserMapper() {
+        return userMapper;
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public User getUser() {
+        User user = new User();
+        user.setId("u1");
+        user.setName("Pocoyo");
+        return userMapper.getUser(user);
+    }
+
+    public List<User> getUsers() {
+        return userMapper.getUsers();
+    }
+
+}
---
+++ src/test/java/sample/User.java
@@ -0,0 +1,55 @@
+/*
+ *    Copyright 2010 The myBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package sample;
+
+/**
+ * 
+ *
+ * @version $Id$
+ */
+public class User {
+
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        StringBuilder buf = new StringBuilder(30);
+        buf.append( "{" );
+        buf.append( id );
+        buf.append( ", " );
+        buf.append( name );
+        buf.append( "}" );
+        return buf.toString();
+    }
+
+}
