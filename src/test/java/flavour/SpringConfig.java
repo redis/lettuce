b991e2db533c02f1fbcfb8ceaba9c92088a9b135
Simone Tripodi <simone.tripodi@gmail.com>
2010-09-14 15:43:13 +0000

    http://code.google.com/p/mybatis/issues/detail?id=99 : junit tests by Raj Nagappan STILL NOT WORKING

---
+++ src/test/java/flavour/SpringConfig.java
@@ -0,0 +1,79 @@
+/*
+ *    Copyright 2010 The myBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package flavour;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.MapperFactoryBean;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * 
+ * @version $Id$
+ */
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    private final SqlSessionFactory sessionFactory;
+
+    public SpringConfig() throws Exception {
+        this.dataSource = new PooledDataSource("org.hsqldb.jdbcDriver", "jdbc:hsqldb:file:mybatisspring", "sa", "");
+
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setConfigLocation(new ClassPathResource("flavour/SpringMybatis.xml"));
+        factoryBean.setDataSource(this.dataSource);
+        this.sessionFactory = factoryBean.getObject();
+    }
+
+    protected <T> T getMapper(Class<T> mapperType) throws Exception {
+        MapperFactoryBean<T> factory = new MapperFactoryBean<T>();
+        factory.setMapperInterface(mapperType);
+        factory.setSqlSessionFactory(this.sessionFactory);
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+    @Bean
+    public WaterMapper waterMapper() throws Exception {
+        return getMapper(WaterMapper.class);
+    }
+
+    @Bean
+    public FlavourMapper flavourMapper() throws Exception {
+        return getMapper(FlavourMapper.class);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        return this.sessionFactory;
+    }
+
+    /*
+     * Use this datasource for an in-memory HSQL DB
+     */
+    @Bean
+    public DataSource dataSource() throws Exception {
+        return this.dataSource;
+    }
+
+}
---
+++ src/test/java/flavour/Water.java
@@ -0,0 +1,68 @@
+/*
+ *    Copyright 2010 The myBatis Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package flavour;
+
+/**
+ * 
+ * @version $Id$
+ */
+public class Water {
+
+    private int id;
+
+    private int size;
+
+    private int flavourId;
+
+    private Flavour flavour;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getFlavourId() {
+        return flavourId;
+    }
+
+    public void setFlavourId(int flavourId) {
+        this.flavourId = flavourId;
+    }
+
+    public Flavour getFlavour() {
+        return flavour;
+    }
+
+    public void setFlavour(Flavour flavour) {
+        this.flavour = flavour;
+    }
+
+    public String getFlavourName() {
+        return getFlavour().getName();
+    }
+
+}
