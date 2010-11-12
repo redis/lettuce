00745b25de99097b950f3d571f45fc971dbd948c
Simone Tripodi <simone.tripodi@gmail.com>
2010-11-12 14:57:40 +0000

    first sentence always has to terminate with a '.'

--- src/main/java/org/mybatis/spring/annotation/Mapper.java
+++ src/main/java/org/mybatis/spring/annotation/Mapper.java
@@ -22,7 +22,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation to identify mapper interfaces for scanning
+ * Annotation to identify mapper interfaces for scanning.
  *
  * @see MapperScannerPostProcessor
  * @version $Id$
