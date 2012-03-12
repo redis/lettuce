a9a4649895d6661d0245964123f8846b3efc6c56
Simone Tripodi <simone.tripodi@gmail.com>
2012-03-12 09:58:54 +0000

    imported static methods

--- src/main/java/org/mybatis/spring/support/SqlSessionDaoSupport.java
+++ src/main/java/org/mybatis/spring/support/SqlSessionDaoSupport.java
@@ -15,12 +15,13 @@
  */
 package org.mybatis.spring.support;
 
+import static org.springframework.util.Assert.*;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DaoSupport;
-import org.springframework.util.Assert;
 
 /**
  * Convenient super class for MyBatis SqlSession data access objects.
@@ -57,8 +58,8 @@ public abstract class SqlSessionDaoSupport extends DaoSupport {
    * Users should use this method to get a SqlSession to call its statement methods
    * This is SqlSession is managed by spring. Users should not commit/rollback/close it
    * because it will be automatically done.
-   * 
-   * @return Spring managed thread safe SqlSession 
+   *
+   * @return Spring managed thread safe SqlSession
    */
   public final SqlSession getSqlSession() {
     return this.sqlSession;
@@ -68,7 +69,7 @@ public abstract class SqlSessionDaoSupport extends DaoSupport {
    * {@inheritDoc}
    */
   protected void checkDaoConfig() {
-    Assert.notNull(this.sqlSession, "Property 'sqlSessionFactory' or 'sqlSessionTemplate' are required");
+    notNull(this.sqlSession, "Property 'sqlSessionFactory' or 'sqlSessionTemplate' are required");
   }
 
 }
