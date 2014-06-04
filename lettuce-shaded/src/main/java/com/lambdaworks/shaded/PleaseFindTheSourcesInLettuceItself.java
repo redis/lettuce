c57b1620064e39a1c7f014eec405db6864681973
Mark Paluch <mpaluch@paluch.biz>
2014-06-04 09:00:29 +0200

    Release preparation

---
+++ lettuce-shaded/src/main/java/com/lambdaworks/shaded/PleaseFindTheSourcesInLettuceItself.java
@@ -0,0 +1,11 @@
+package com.lambdaworks.shaded;
+
+/**
+ * This is a dummy class. Please find the sources and javadoc in the lettuce client itself.
+ *
+ * @author <a href="mailto:mark.paluch@1und1.de">Mark Paluch</a>
+ * @since 04.06.14 08:58
+ */
+public class PleaseFindTheSourcesInLettuceItself
+{
+}
