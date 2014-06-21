b07a2f084248f79b4a2794a1ec2441a359511b75
Mark Paluch <mpaluch@paluch.biz>
2014-06-21 17:40:41 +0200

    Fixed clientKill because of changed clientList output

--- lettuce/src/test/java/com/lambdaworks/redis/ServerCommandTest.java
+++ lettuce/src/test/java/com/lambdaworks/redis/ServerCommandTest.java
@@ -48,11 +48,10 @@ public class ServerCommandTest extends AbstractCommandTest {
 
     @Test
     public void clientKill() throws Exception {
-        Pattern p = Pattern.compile("addr=([^ ]+)");
+        Pattern p = Pattern.compile(".*addr=([^ ]+).*");
         String clients = redis.clientList();
         Matcher m = p.matcher(clients);
 
-        log.info("Client List: " + clients);
         assertTrue(m.lookingAt());
         assertEquals("OK", redis.clientKill(m.group(1)));
     }
