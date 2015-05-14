1acda9f38793355f549e8a6a1a0d006d9f85a499
Mark Paluch <mpaluch@paluch.biz>
2015-05-14 15:43:44 +0200

    Extend tests

---
+++ src/test/java/com/lambdaworks/redis/ClientOptionsTest.java
@@ -0,0 +1,34 @@
+package com.lambdaworks.redis;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
+ */
+public class ClientOptionsTest {
+
+    @Test
+    public void testNew() throws Exception {
+        checkAssertions(new ClientOptions());
+    }
+
+    @Test
+    public void testBuilder() throws Exception {
+        checkAssertions(new ClientOptions.Builder().build());
+    }
+
+    @Test
+    public void testCopy() throws Exception {
+        checkAssertions(ClientOptions.copyOf(new ClientOptions.Builder().build()));
+    }
+
+    protected void checkAssertions(ClientOptions sut) {
+        assertThat(sut.isAutoReconnect()).isEqualTo(true);
+        assertThat(sut.isCancelCommandsOnReconnectFailure()).isEqualTo(false);
+        assertThat(sut.isPingBeforeActivateConnection()).isEqualTo(false);
+        assertThat(sut.isSuspendReconnectOnProtocolFailure()).isEqualTo(false);
+    }
+
+}
