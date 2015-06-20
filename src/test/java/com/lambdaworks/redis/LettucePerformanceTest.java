696d200b5849d42c268b0ec5d9141af76631c0ba
Mark Paluch <mpaluch@paluch.biz>
2015-06-20 22:07:35 +0200

    Add performance test for reactive commands

--- src/test/java/com/lambdaworks/redis/LettucePerformanceTest.java
+++ src/test/java/com/lambdaworks/redis/LettucePerformanceTest.java
@@ -2,25 +2,57 @@ package com.lambdaworks.redis;
 
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
+import rx.Observable;
+
 import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
+import com.lambdaworks.CapturingLogAppender;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.rx.RedisReactiveCommands;
 
 /**
  * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
  */
 public class LettucePerformanceTest {
 
-    private RedisClient redisClient = DefaultRedisClient.get();
+    private static RedisClient redisClient = new RedisClient(TestSettings.host(), TestSettings.port());
     private ExecutorService executor;
     private CountDownLatch latch = new CountDownLatch(1);
+
+    @Before
+    public void before() throws Exception {
+        LogManager.resetConfiguration();
+        LogManager.getRootLogger().setLevel(Level.WARN);
+        CapturingLogAppender.disable();
+    }
+
+    @After
+    public void after() throws Exception {
+        CapturingLogAppender.enable();
+        PropertyConfigurator.configure(Resources.getResource("log4j.properties"));
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.MINUTES);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        redisClient.shutdown();
+    }
 
     /**
      * Multi-threaded performance test.
@@ -31,7 +63,7 @@ public class LettucePerformanceTest {
      * @throws Exception
      */
     @Test
-    public void testPerformance() throws Exception {
+    public void testSyncAsyncPerformance() throws Exception {
 
         // TWEAK ME
         int threads = 4;
@@ -44,7 +76,7 @@ public class LettucePerformanceTest {
 
         executor = new ThreadPoolExecutor(threads, threads, 1, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(totalCalls));
 
-        List<Future<List<RedisFuture<String>>>> futurama = Lists.newArrayList();
+        List<Future<List<CompletableFuture<String>>>> futurama = Lists.newArrayList();
 
         preheat(threads);
 
@@ -56,8 +88,8 @@ public class LettucePerformanceTest {
         long start = System.currentTimeMillis();
         latch.countDown();
 
-        for (Future<List<RedisFuture<String>>> listFuture : futurama) {
-            for (RedisFuture<String> future : listFuture.get()) {
+        for (Future<List<CompletableFuture<String>>> listFuture : futurama) {
+            for (CompletableFuture<String> future : listFuture.get()) {
                 if (waitForFutureCompletion) {
                     future.get();
                 }
@@ -69,12 +101,18 @@ public class LettucePerformanceTest {
         long duration = end - start;
         double durationSeconds = duration / 1000d;
         double opsPerSecond = totalCalls / durationSeconds;
-        System.out.println(String.format("Duration: %d ms (%.2f sec), operations: %d, %.2f ops/sec ", duration,
+        System.out.println(String.format("Sync/Async: Duration: %d ms (%.2f sec), operations: %d, %.2f ops/sec ", duration,
                 durationSeconds, totalCalls, opsPerSecond));
+
+        for (Future<List<CompletableFuture<String>>> listFuture : futurama) {
+            for (CompletableFuture<String> future : listFuture.get()) {
+                future.get();
+            }
+        }
 
     }
 
-    protected void submitExecutionTasks(int threads, List<Future<List<RedisFuture<String>>>> futurama,
+    protected void submitExecutionTasks(int threads, List<Future<List<CompletableFuture<String>>>> futurama,
             final int callsPerThread, final boolean connectionPerThread) {
         final RedisAsyncConnection<String, String> sharedConnection;
         if (!connectionPerThread) {
@@ -84,24 +122,107 @@ public class LettucePerformanceTest {
         }
 
         for (int i = 0; i < threads; i++) {
-            Future<List<RedisFuture<String>>> submit = executor.submit(new Callable<List<RedisFuture<String>>>() {
-                @Override
-                public List<RedisFuture<String>> call() throws Exception {
+            Future<List<CompletableFuture<String>>> submit = executor.submit(() -> {
 
-                    RedisAsyncConnection<String, String> connection = sharedConnection;
-                    if (connectionPerThread) {
-                        connection = redisClient.connectAsync();
-                    }
-                    connection.ping().get();
-
-                    List<RedisFuture<String>> futures = Lists.newArrayListWithCapacity(callsPerThread);
-                    latch.await();
-                    for (int i = 0; i < callsPerThread; i++) {
-                        futures.add(connection.ping());
-                    }
-
-                    return futures;
+                RedisAsyncConnection<String, String> connection = sharedConnection;
+                if (connectionPerThread) {
+                    connection = redisClient.connectAsync();
                 }
+                connection.ping().get();
+
+                List<CompletableFuture<String>> futures = Lists.newArrayListWithCapacity(callsPerThread);
+                latch.await();
+                for (int i1 = 0; i1 < callsPerThread; i1++) {
+                    futures.add(connection.ping().toCompletableFuture());
+                }
+
+                return futures;
+            });
+
+            futurama.add(submit);
+        }
+    }
+
+    /**
+     * Multi-threaded performance using reactive commands.
+     *
+     * Uses a {@link ThreadPoolExecutor} with thread and connection preheating. Execution tasks are submitted and synchronized
+     * with a {@link CountDownLatch}
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testObservablePerformance() throws Exception {
+
+        // TWEAK ME
+        int threads = 4;
+        int totalCalls = 25000;
+        boolean waitForCompletion = true;
+        boolean connectionPerThread = false;
+        // Keep in mind, that the size of the event loop threads is CPU count * 4 unless you
+        // set -Dio.netty.eventLoopThreads=...
+        // END OF TWEAK ME
+
+        executor = new ThreadPoolExecutor(threads, threads, 1, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(totalCalls));
+
+        List<Future<List<Observable<String>>>> futurama = Lists.newArrayList();
+
+        preheat(threads);
+        final int callsPerThread = totalCalls / threads;
+
+        submitObservableTasks(threads, futurama, callsPerThread, connectionPerThread);
+        Thread.sleep(800);
+
+        long start = System.currentTimeMillis();
+        latch.countDown();
+
+        for (Future<List<Observable<String>>> listFuture : futurama) {
+            for (Observable<String> future : listFuture.get()) {
+                if (waitForCompletion) {
+                    future.toBlocking().last();
+                } else {
+                    future.subscribe();
+                }
+            }
+        }
+
+        long end = System.currentTimeMillis();
+
+        long duration = end - start;
+        double durationSeconds = duration / 1000d;
+        double opsPerSecond = totalCalls / durationSeconds;
+        System.out.println(String.format("Reactive Duration: %d ms (%.2f sec), operations: %d, %.2f ops/sec ", duration,
+                durationSeconds, totalCalls, opsPerSecond));
+
+    }
+
+    protected void submitObservableTasks(int threads, List<Future<List<Observable<String>>>> futurama,
+            final int callsPerThread, final boolean connectionPerThread) {
+        final StatefulRedisConnection<String, String> sharedConnection;
+        if (!connectionPerThread) {
+            sharedConnection = redisClient.connectAsync().getStatefulConnection();
+        } else {
+            sharedConnection = null;
+        }
+
+        for (int i = 0; i < threads; i++) {
+            Future<List<Observable<String>>> submit = executor.submit(() -> {
+
+                StatefulRedisConnection<String, String> connection = sharedConnection;
+                if (connectionPerThread) {
+                    connection = redisClient.connectAsync().getStatefulConnection();
+                }
+                RedisReactiveCommands<String, String> reactive = connection.reactive();
+
+                connection.sync().ping();
+
+                List<Observable<String>> observables = Lists.newArrayListWithCapacity(callsPerThread);
+                latch.await();
+                for (int i1 = 0; i1 < callsPerThread; i1++) {
+                    observables.add(reactive.ping());
+                }
+
+                return observables;
             });
 
             futurama.add(submit);
