4b5a31b51fa5dd68bf93481d057352d487e9e8f3
Mark Paluch <mpaluch@paluch.biz>
2015-07-20 12:51:42 +0200

    Prepare release notes

--- src/main/java/com/lambdaworks/redis/cluster/RedisAdvancedClusterConnection.java
+++ src/main/java/com/lambdaworks/redis/cluster/RedisAdvancedClusterConnection.java
@@ -1,6 +1,7 @@
 package com.lambdaworks.redis.cluster;
 
 import com.lambdaworks.redis.RedisClusterConnection;
+import com.lambdaworks.redis.RedisException;
 
 /**
  * Advanced synchronous cluster API.
@@ -12,18 +13,31 @@ public interface RedisAdvancedClusterConnection<K, V> extends RedisClusterConnec
 
     /**
      * Retrieve a connection to the specified cluster node using the nodeId. Host and port are looked up in the node list.
-     * 
+     *
+     * Do not close the connections. Otherwise, unpredictable behavior will occur. The nodeId must be part of the cluster and is
+     * validated against the current topology view in {@link com.lambdaworks.redis.cluster.models.partitions.Partitions}.
+     *
+     * In contrast to the {@link RedisAdvancedClusterConnection}, node-connections do not route commands to other cluster
+     * nodes.
+     *
      * @param nodeId the node Id
      * @return a connection to the requested cluster node
+     * @throws RedisException if the requested node identified by {@code nodeId} is not part of the cluster
      */
     RedisClusterConnection<K, V> getConnection(String nodeId);
 
     /**
-     * Retrieve a connection to the specified cluster node using the nodeId.
+     * Retrieve a connection to the specified cluster node using the nodeId. Do not close the connections. Otherwise,
+     * unpredictable behavior will occur. The node must be part of the cluster and host/port are validated (exact check) against
+     * the current topology view in {@link com.lambdaworks.redis.cluster.models.partitions.Partitions}.
+     *
+     * In contrast to the {@link RedisAdvancedClusterConnection}, node-connections do not route commands to other cluster
+     * nodes.
      * 
      * @param host the host
      * @param port the port
      * @return a connection to the requested cluster node
+     * @throws RedisException if the requested node identified by {@code host} and {@code port} is not part of the cluster
      */
     RedisClusterConnection<K, V> getConnection(String host, int port);
 }
