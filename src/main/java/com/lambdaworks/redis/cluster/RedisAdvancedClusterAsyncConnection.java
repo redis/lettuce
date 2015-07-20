1818ee123056c8b8a815d67576071b6f40f79786
Mark Paluch <mpaluch@paluch.biz>
2015-07-20 12:55:36 +0200

    Documentation

--- src/main/java/com/lambdaworks/redis/cluster/RedisAdvancedClusterAsyncConnection.java
+++ src/main/java/com/lambdaworks/redis/cluster/RedisAdvancedClusterAsyncConnection.java
@@ -1,11 +1,8 @@
 package com.lambdaworks.redis.cluster;
 
-import java.util.function.Predicate;
-
 import com.lambdaworks.redis.RedisClusterAsyncConnection;
-import com.lambdaworks.redis.cluster.api.NodeSelection;
+import com.lambdaworks.redis.RedisException;
 import com.lambdaworks.redis.cluster.api.StatefulRedisClusterConnection;
-import com.lambdaworks.redis.cluster.models.partitions.RedisClusterNode;
 
 /**
  * Advanced asynchronous and thread-safe cluster API.
@@ -20,21 +17,30 @@ public interface RedisAdvancedClusterAsyncConnection<K, V> extends RedisClusterA
     /**
      * Retrieve a connection to the specified cluster node using the nodeId. Host and port are looked up in the node list.
      * 
+     * Do not close the connections. Otherwise, unpredictable behavior will occur. The nodeId must be part of the cluster and is
+     * validated against the current topology view in {@link com.lambdaworks.redis.cluster.models.partitions.Partitions}.
+     * 
      * In contrast to the {@link RedisAdvancedClusterAsyncConnection}, node-connections do not route commands to other cluster
      * nodes
      * 
      * @param nodeId the node Id
      * @return a connection to the requested cluster node
+     * @throws RedisException if the requested node identified by {@code nodeId} is not part of the cluster
      */
     RedisClusterAsyncConnection<K, V> getConnection(String nodeId);
 
     /**
-     * Retrieve a connection to the specified cluster node using the nodeId. In contrast to the
-     * {@link RedisAdvancedClusterAsyncConnection}, node-connections do not route commands to other cluster nodes
+     * Retrieve a connection to the specified cluster node using the nodeId. Do not close the connections. Otherwise,
+     * unpredictable behavior will occur. The node must be part of the cluster and host/port are validated (exact check) against
+     * the current topology view in {@link com.lambdaworks.redis.cluster.models.partitions.Partitions}.
+     * 
+     * In contrast to the {@link RedisAdvancedClusterAsyncConnection}, node-connections do not route commands to other cluster
+     * nodes.
      * 
      * @param host the host
      * @param port the port
      * @return a connection to the requested cluster node
+     * @throws RedisException if the requested node identified by {@code host} and {@code port} is not part of the cluster
      */
     RedisClusterAsyncConnection<K, V> getConnection(String host, int port);
 
