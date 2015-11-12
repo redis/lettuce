d937e87df58d3eff2b73656156c048511f1ccca7
Sergey Ushakov <sushakov@marathonbet.ru>
2015-11-12 12:59:12 +0600

    Added configurable timeout to connection string #152

--- src/test/java/com/lambdaworks/redis/RedisURITest.java
+++ src/test/java/com/lambdaworks/redis/RedisURITest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -84,5 +85,45 @@ public class RedisURITest {
 
         assertThat(redisURI3).isNotEqualTo(redisURI2);
         assertThat(redisURI3.hashCode()).isNotEqualTo(redisURI2.hashCode());
+    }
+
+    @Test
+    public void timeoutParsingTest() throws Exception {
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=5000", 5000, TimeUnit.MILLISECONDS);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=5000ms", 5000, TimeUnit.MILLISECONDS);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=5s", 5, TimeUnit.SECONDS);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=100us", 100, TimeUnit.MICROSECONDS);
+        checkUriTimeout("redis://auth@localhost:1234/5?TIMEOUT=1000000NS", 1000000, TimeUnit.NANOSECONDS);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=60m", 60, TimeUnit.MINUTES);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=24h", 24, TimeUnit.HOURS);
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=1d", 1, TimeUnit.DAYS);
+
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=-1", 0, TimeUnit.MILLISECONDS);
+
+        RedisURI defaultUri = new RedisURI();
+        checkUriTimeout("redis://auth@localhost:1234/5?timeout=junk", defaultUri.getTimeout(), defaultUri.getUnit());
+    }
+
+    @Test
+    public void queryStringDecodingTest() throws Exception {
+        String timeout = "%74%69%6D%65%6F%75%74";
+        String eq = "%3d";
+        String s = "%73";
+        checkUriTimeout("redis://auth@localhost:1234/5?" + timeout + eq + "5" + s, 5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void timeoutParsingWithJunkParamTest() throws Exception {
+        RedisURI redisURI1 = RedisURI.create("redis-sentinel://auth@localhost:1234/5?timeout=5s;junkparam=#master-instance");
+        assertThat(redisURI1.getTimeout()).isEqualTo(5);
+        assertThat(redisURI1.getUnit()).isEqualTo(TimeUnit.SECONDS);
+        assertThat(redisURI1.getSentinelMasterId()).isEqualTo("master-instance");
+    }
+
+    private RedisURI checkUriTimeout(String uri, long expectedTimeout, TimeUnit expectedUnit) {
+        RedisURI redisURI1 = RedisURI.create(uri);
+        assertThat(redisURI1.getTimeout()).isEqualTo(expectedTimeout);
+        assertThat(redisURI1.getUnit()).isEqualTo(expectedUnit);
+        return redisURI1;
     }
 }
