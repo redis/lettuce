5da66ce7f3a31ad9355bd48314a998328066681d
Mark Paluch <mpaluch@paluch.biz>
2016-02-08 21:12:57 +0100

    Include pub/sub commands for node command routing #138
    Add the pub/sub command types to the ReadOnly command enumeration for Redis Cluster to allow dispatching of pub/sub commands to Redis Cluster slaves

--- src/test/java/com/lambdaworks/redis/cluster/ReadOnlyCommandsTest.java
+++ src/test/java/com/lambdaworks/redis/cluster/ReadOnlyCommandsTest.java
@@ -2,6 +2,8 @@ package com.lambdaworks.redis.cluster;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.lambdaworks.redis.protocol.CommandType;
+import com.lambdaworks.redis.protocol.ProtocolKeyword;
 import org.junit.Test;
 
 /**
@@ -11,6 +13,13 @@ public class ReadOnlyCommandsTest {
 
     @Test
     public void testCount() throws Exception {
-        assertThat(ReadOnlyCommands.READ_ONLY_COMMANDS).hasSize(62);
+        assertThat(ReadOnlyCommands.READ_ONLY_COMMANDS).hasSize(68);
+    }
+
+    @Test
+    public void testResolvableCommandNames() throws Exception {
+        for (ProtocolKeyword readOnlyCommand : ReadOnlyCommands.READ_ONLY_COMMANDS) {
+            assertThat(readOnlyCommand.name()).isEqualTo(CommandType.valueOf(readOnlyCommand.name()).name());
+        }
     }
 }
