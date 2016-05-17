bd9e6bbaf96426814792fd0ee7268bfe17e96c48
Mark Paluch <mpaluch@paluch.biz>
2016-05-17 13:19:42 +0200

    Throw RedisCommandExecutionException when awaiting commands #242
    LettuceFutures.awaitAll throws now RedisCommandExecutionException when a failed command is synchronized and RedisCommandInterruptedException when running into InterruptedExceptions.

--- src/main/java/com/lambdaworks/redis/RedisCommandExecutionException.java
+++ src/main/java/com/lambdaworks/redis/RedisCommandExecutionException.java
@@ -2,11 +2,15 @@ package com.lambdaworks.redis;
 
 /**
  * Exception for errors states reported by Redis.
- * 
+ *
  * @author <a href="mailto:mpaluch@paluch.biz">Mark Paluch</a>
  */
 @SuppressWarnings("serial")
 public class RedisCommandExecutionException extends RedisException {
+
+    public RedisCommandExecutionException(Throwable cause) {
+        super(cause);
+    }
 
     public RedisCommandExecutionException(String msg) {
         super(msg);
