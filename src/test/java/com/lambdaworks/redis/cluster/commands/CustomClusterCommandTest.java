993e4c713792457310916aabe41c718289ba47d7
Mark Paluch <mpaluch@paluch.biz>
2016-05-28 15:44:46 +0200

    Add enum for a custom command.

--- src/test/java/com/lambdaworks/redis/cluster/commands/CustomClusterCommandTest.java
+++ src/test/java/com/lambdaworks/redis/cluster/commands/CustomClusterCommandTest.java
@@ -1,26 +1,34 @@
 package com.lambdaworks.redis.cluster.commands;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.junit.Test;
 
-import com.lambdaworks.redis.FastShutdown;
-import com.lambdaworks.redis.RedisURI;
-import com.lambdaworks.redis.TestSettings;
-import com.lambdaworks.redis.api.sync.RedisCommands;
+import com.lambdaworks.redis.*;
+import com.lambdaworks.redis.cluster.AbstractClusterTest;
 import com.lambdaworks.redis.cluster.ClusterTestUtil;
 import com.lambdaworks.redis.cluster.RedisClusterClient;
 import com.lambdaworks.redis.cluster.api.StatefulRedisClusterConnection;
+import com.lambdaworks.redis.cluster.api.sync.RedisAdvancedClusterCommands;
+import com.lambdaworks.redis.codec.Utf8StringCodec;
 import com.lambdaworks.redis.commands.CustomCommandTest;
+import com.lambdaworks.redis.output.StatusOutput;
+import com.lambdaworks.redis.protocol.*;
+
+import rx.Observable;
 
 /**
  * @author Mark Paluch
  */
-public class CustomClusterCommandTest extends CustomCommandTest {
+public class CustomClusterCommandTest extends AbstractClusterTest {
 
+    private static final Utf8StringCodec utf8StringCodec = new Utf8StringCodec();
     private static RedisClusterClient redisClusterClient;
-    private StatefulRedisClusterConnection<String, String> clusterConnection;
+    private StatefulRedisClusterConnection<String, String> redisClusterConnection;
+    private RedisAdvancedClusterCommands<String, String> redis;
 
     @BeforeClass
     public static void setupClient() {
@@ -35,19 +43,84 @@ public class CustomClusterCommandTest extends CustomCommandTest {
 
     @Before
     public void openConnection() throws Exception {
-        redis = connect();
-        ClusterTestUtil.flushDatabaseOfAllNodes(clusterConnection);
+        redisClusterConnection = redisClusterClient.connect();
+        redis = redisClusterConnection.sync();
+        ClusterTestUtil.flushDatabaseOfAllNodes(redisClusterConnection);
     }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    protected RedisCommands<String, String> connect() {
-        clusterConnection = redisClusterClient.connectCluster().getStatefulConnection();
-        return ClusterTestUtil.redisCommandsOverCluster(clusterConnection);
+    @Test
+    public void dispatchSet() throws Exception {
+
+        String response = redis.dispatch(CustomCommandTest.MyCommands.SET, new StatusOutput<>(utf8StringCodec),
+                new CommandArgs<>(utf8StringCodec).addKey(key).addValue(value));
+
+        assertThat(response).isEqualTo("OK");
     }
 
-    @Ignore("MULTI not available on Redis Cluster")
-    @Override
-    public void dispatchTransactions() throws Exception {
+    @Test
+    public void dispatchWithoutArgs() throws Exception {
+
+        String response = redis.dispatch(CustomCommandTest.MyCommands.INFO, new StatusOutput<>(utf8StringCodec));
+
+        assertThat(response).contains("connected_clients");
     }
+
+    @Test(expected = RedisCommandExecutionException.class)
+    public void dispatchShouldFailForWrongDataType() throws Exception {
+
+        redis.hset(key, key, value);
+        redis.dispatch(CommandType.GET, new StatusOutput<>(utf8StringCodec), new CommandArgs<>(utf8StringCodec).addKey(key));
+    }
+
+    @Test
+    public void standaloneAsyncPing() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(CustomCommandTest.MyCommands.PING,
+                new StatusOutput<>(new Utf8StringCodec()), null);
+
+        AsyncCommand<String, String, String> async = new AsyncCommand<>(command);
+        redisClusterConnection.dispatch(async);
+
+        assertThat(async.get()).isEqualTo("PONG");
+    }
+
+    @Test
+    public void standaloneFireAndForget() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(CustomCommandTest.MyCommands.PING,
+                new StatusOutput<>(new Utf8StringCodec()), null);
+        redisClusterConnection.dispatch(command);
+        assertThat(command.isCancelled()).isFalse();
+
+    }
+
+    @Test
+    public void standaloneReactivePing() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(CustomCommandTest.MyCommands.PING,
+                new StatusOutput<>(new Utf8StringCodec()), null);
+        ReactiveCommandDispatcher<String, String, String> dispatcher = new ReactiveCommandDispatcher<>(command,
+                redisClusterConnection, false);
+
+        String result = Observable.create(dispatcher).toBlocking().first();
+
+        assertThat(result).isEqualTo("PONG");
+    }
+
+    public enum MyCommands implements ProtocolKeyword {
+        PING, SET, INFO;
+
+        private final byte name[];
+
+        MyCommands() {
+            // cache the bytes for the command name. Reduces memory and cpu pressure when using commands.
+            name = name().getBytes();
+        }
+
+        @Override
+        public byte[] getBytes() {
+            return name;
+        }
+    }
+
 }
--- src/test/java/com/lambdaworks/redis/commands/CustomCommandTest.java
+++ src/test/java/com/lambdaworks/redis/commands/CustomCommandTest.java
@@ -1,17 +1,22 @@
 package com.lambdaworks.redis.commands;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 
 import java.util.List;
 
 import org.junit.Test;
 
 import com.lambdaworks.redis.AbstractRedisClientTest;
+import com.lambdaworks.redis.ReactiveCommandDispatcher;
 import com.lambdaworks.redis.RedisCommandExecutionException;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.cluster.api.StatefulRedisClusterConnection;
 import com.lambdaworks.redis.codec.Utf8StringCodec;
 import com.lambdaworks.redis.output.StatusOutput;
-import com.lambdaworks.redis.protocol.CommandArgs;
-import com.lambdaworks.redis.protocol.CommandType;
+import com.lambdaworks.redis.protocol.*;
+
+import rx.Observable;
 
 /**
  * @author Mark Paluch
@@ -23,7 +28,7 @@ public class CustomCommandTest extends AbstractRedisClientTest {
     @Test
     public void dispatchSet() throws Exception {
 
-        String response = redis.dispatch(CommandType.SET, new StatusOutput<>(utf8StringCodec),
+        String response = redis.dispatch(MyCommands.SET, new StatusOutput<>(utf8StringCodec),
                 new CommandArgs<>(utf8StringCodec).addKey(key).addValue(value));
 
         assertThat(response).isEqualTo("OK");
@@ -32,7 +37,7 @@ public class CustomCommandTest extends AbstractRedisClientTest {
     @Test
     public void dispatchWithoutArgs() throws Exception {
 
-        String response = redis.dispatch(CommandType.INFO, new StatusOutput<>(utf8StringCodec));
+        String response = redis.dispatch(MyCommands.INFO, new StatusOutput<>(utf8StringCodec));
 
         assertThat(response).contains("connected_clients");
     }
@@ -55,5 +60,62 @@ public class CustomCommandTest extends AbstractRedisClientTest {
 
         assertThat(response).isNull();
         assertThat(exec).hasSize(1).contains("OK");
+    }
+
+    @Test
+    public void standaloneAsyncPing() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(MyCommands.PING, new StatusOutput<>(new Utf8StringCodec()),
+                null);
+
+        AsyncCommand<String, String, String> async = new AsyncCommand<>(command);
+        getStandaloneConnection().dispatch(async);
+
+        assertThat(async.get()).isEqualTo("PONG");
+    }
+
+    @Test
+    public void standaloneFireAndForget() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(MyCommands.PING, new StatusOutput<>(new Utf8StringCodec()),
+                null);
+        getStandaloneConnection().dispatch(command);
+        assertThat(command.isCancelled()).isFalse();
+
+    }
+
+    @Test
+    public void standaloneReactivePing() throws Exception {
+
+        RedisCommand<String, String, String> command = new Command<>(MyCommands.PING, new StatusOutput<>(new Utf8StringCodec()),
+                null);
+        ReactiveCommandDispatcher<String, String, String> dispatcher = new ReactiveCommandDispatcher<>(command,
+                getStandaloneConnection(), false);
+
+        String result = Observable.create(dispatcher).toBlocking().first();
+
+        assertThat(result).isEqualTo("PONG");
+    }
+
+    private StatefulRedisConnection<String, String> getStandaloneConnection() {
+
+        assumeTrue(redis.getStatefulConnection() instanceof StatefulRedisConnection);
+        return redis.getStatefulConnection();
+    }
+
+    public enum MyCommands implements ProtocolKeyword {
+        PING, SET, INFO;
+
+        private final byte name[];
+
+        MyCommands() {
+            // cache the bytes for the command name. Reduces memory and cpu pressure when using commands.
+            name = name().getBytes();
+        }
+
+        @Override
+        public byte[] getBytes() {
+            return name;
+        }
     }
 }
