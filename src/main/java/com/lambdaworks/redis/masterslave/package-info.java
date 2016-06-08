caab7b1b937176cd1e31d7f64758c25f3db83957
Mark Paluch <mpaluch@paluch.biz>
2016-06-08 23:48:17 +0200

    Add missing package-info #263

---
+++ src/main/java/com/lambdaworks/redis/masterslave/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Client support for Redis Master/Slave setups. {@link com.lambdaworks.redis.masterslave.MasterSlave} supports self-managed,
+ * Redis Sentinel-managed, AWS ElastiCache and Azure Redis managed Master/Slave setups.
+ */
+package com.lambdaworks.redis.masterslave;
