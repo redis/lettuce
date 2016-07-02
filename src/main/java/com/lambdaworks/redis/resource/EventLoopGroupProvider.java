05f9fa7663f6f4fc35d7a4b9e3f1495e92e59fcc
Mark Paluch <mpaluch@paluch.biz>
2016-07-02 18:41:29 +0200

    Polishing
    Improve Javadoc of ClientResources and EventLoopGroupProvider.

--- src/main/java/com/lambdaworks/redis/resource/EventLoopGroupProvider.java
+++ src/main/java/com/lambdaworks/redis/resource/EventLoopGroupProvider.java
@@ -7,18 +7,21 @@ import io.netty.util.concurrent.EventExecutorGroup;
 import io.netty.util.concurrent.Future;
 
 /**
- * Provider for {@link EventExecutorGroup EventLoopGroups and EventExecutorGroups}. A event loop group is a heavy-weight
- * instance holding and providing {@link Thread} instances. Multiple instances can be created but are expensive. Keeping too
- * many instances open can exhaust the number of open files.
+ * Provider for {@link EventLoopGroup EventLoopGroups and EventExecutorGroups}. A event loop group is a heavy-weight instance
+ * holding and providing {@link Thread} instances. Multiple instances can be created but are expensive. Keeping too many
+ * instances open can exhaust the number of open files.
  * <p>
  * Usually, the default settings are sufficient. However, customizing might be useful for some special cases where multiple
- * RedisClient or RedisClusterClient instances are needed that share one or more event loop groups.
+ * {@link com.lambdaworks.redis.RedisClient} or {@link com.lambdaworks.redis.cluster.RedisClusterClient} instances are needed
+ * that share one or more event loop groups.
  * </p>
  * <p>
  * The {@link EventLoopGroupProvider} allows to allocate and release instances implementing {@link EventExecutorGroup}. The
  * {@link EventExecutorGroup} instances must not be terminated or shutdown by the user. Resources are managed by the particular
  * {@link EventLoopGroupProvider}.
  * </p>
+ * You can implement your own {@link EventLoopGroupProvider} to share existing {@link EventLoopGroup EventLoopGroup's} with
+ * lettuce.
  * 
  * @author Mark Paluch
  * @since 3.4
@@ -26,12 +29,12 @@ import io.netty.util.concurrent.Future;
 public interface EventLoopGroupProvider {
 
     /**
-     * Retrieve a {@link EventLoopGroup} for the type {@code type}. Do not terminate or shutdown the instance. Call the
-     * {@link #release(EventExecutorGroup, long, long, TimeUnit)} to release an individual instance or
+     * Retrieve a {@link EventLoopGroup} for the {@link EventLoopGroup channel} {@code type}. Do not terminate or shutdown the
+     * instance. Call the {@link #release(EventExecutorGroup, long, long, TimeUnit)} to release an individual instance or
      * {@link #shutdown(long, long, TimeUnit)} method to free the all resources.
      *
-     * @param type type of the event loop group, must not be {@literal null}
-     * @param <T> type parameter
+     * @param type class of the {@link EventLoopGroup}, must not be {@literal null}
+     * @param <T> type of the {@link EventLoopGroup}
      * @return the {@link EventLoopGroup}.
      */
     <T extends EventLoopGroup> T allocate(Class<T> type);
@@ -45,7 +48,7 @@ public interface EventLoopGroupProvider {
     int threadPoolSize();
 
     /**
-     * Release a {@code eventLoopGroup} instance. The method will shutdown/terminate the event loop group if it is no longer
+     * Release a {@code eventLoopGroup} instance. The method will shutdown/terminate the {@link EventExecutorGroup} if it is no longer
      * needed.
      * 
      * @param eventLoopGroup the eventLoopGroup instance, must not be {@literal null}
