67d65e92454e2f57cfebfffdfa105debea7aff03
Mark Paluch <mpaluch@paluch.biz>
2016-09-22 14:27:07 +0200

    Polishing.
    Fix JavaDoc method references.

--- src/main/templates/com/lambdaworks/redis/api/RedisGeoCommands.java
+++ src/main/templates/com/lambdaworks/redis/api/RedisGeoCommands.java
@@ -70,7 +70,7 @@ public interface RedisGeoCommands<K, V> {
     List<GeoWithin<V>> georadius(K key, double longitude, double latitude, double distance, GeoArgs.Unit unit, GeoArgs geoArgs);
 
     /**
-     * Perform a {@link #georadius(Object, double, double, double, Unit, GeoArgs)} query and store the results in a sorted set.
+     * Perform a {@link #georadius(Object, double, double, double, GeoArgs.Unit, GeoArgs)} query and store the results in a sorted set.
      *
      * @param key the key of the geo set
      * @param longitude the longitude coordinate according to WGS84
@@ -110,7 +110,7 @@ public interface RedisGeoCommands<K, V> {
     List<GeoWithin<V>> georadiusbymember(K key, V member, double distance, GeoArgs.Unit unit, GeoArgs geoArgs);
 
     /**
-     * Perform a {@link #georadiusbymember(Object, Object, double, Unit, GeoArgs)} query and store the results in a sorted set.
+     * Perform a {@link #georadiusbymember(Object, Object, double, GeoArgs.Unit, GeoArgs)} query and store the results in a sorted set.
      *
      * @param key the key of the geo set
      * @param member reference member
