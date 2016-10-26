42c51e60201c5da3e3f1be379c2f81e29de86cae
Mark Paluch <mpaluch@paluch.biz>
2016-10-26 17:54:05 +0200

    Update license/author headers #387

--- src/main/java/com/lambdaworks/redis/SetArgs.java
+++ src/main/java/com/lambdaworks/redis/SetArgs.java
@@ -1,6 +1,18 @@
-// Copyright (C) 2011 - Will Glozer.  All rights reserved.
-// Copyright (C) 2013 - Vincent Rischmann.  All rights reserved.
-
+/*
+ * Copyright 2011-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.lambdaworks.redis;
 
 import com.lambdaworks.redis.protocol.CommandArgs;
@@ -9,6 +21,7 @@ import com.lambdaworks.redis.protocol.CommandArgs;
  * Argument list builder for the new redis <a href="http://redis.io/commands/set">SET</a> command starting from Redis 2.6.12.
  * Static import the methods from {@link Builder} and chain the method calls: {@code ex(10).nx()}.
  * 
+ * @author Will Glozer
  * @author Vincent Rischmann
  */
 public class SetArgs {
