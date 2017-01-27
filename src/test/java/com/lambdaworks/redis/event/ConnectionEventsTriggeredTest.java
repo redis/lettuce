8906735cc5d825c46b54be64b6eda068f2f88e3c
Mark Paluch <mpaluch@paluch.biz>
2017-01-27 17:48:14 +0100

    Use Reactor's StepVerifier in ConnectionEventsTriggeredTest #455

--- src/test/java/com/lambdaworks/redis/event/ConnectionEventsTriggeredTest.java
+++ src/test/java/com/lambdaworks/redis/event/ConnectionEventsTriggeredTest.java
@@ -17,61 +17,38 @@ package com.lambdaworks.redis.event;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.lambdaworks.redis.reactive.TestSubscriber;
-import org.assertj.core.api.Condition;
 import org.junit.Test;
 
-import com.lambdaworks.Wait;
-import com.lambdaworks.redis.AbstractRedisClientTest;
-import com.lambdaworks.redis.event.connection.*;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import com.lambdaworks.TestClientResources;
+import com.lambdaworks.redis.AbstractTest;
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisURI;
+import com.lambdaworks.redis.event.connection.ConnectionEvent;
 
 /**
  * @author Mark Paluch
  */
-public class ConnectionEventsTriggeredTest extends AbstractRedisClientTest {
+public class ConnectionEventsTriggeredTest extends AbstractTest {
 
     @Test
     public void testConnectionEvents() throws Exception {
 
-        TestSubscriber<Object> subscriber = TestSubscriber.create();
-        List<Object> events = new ArrayList<>();
+        RedisClient client = RedisClient.create(TestClientResources.get(), RedisURI.Builder.redis(host, port).build());
 
-        client.getResources().eventBus().get().filter(event -> event instanceof ConnectionEvent).doOnNext(events::add)
-                .subscribe(subscriber);
+        Flux<ConnectionEvent> publisher = client.getResources().eventBus().get()
+                .filter(event -> event instanceof ConnectionEvent).cast(ConnectionEvent.class);
 
-        try {
-            subscriber.request(10);
-            client.connect().close();
-            Wait.untilTrue(() -> events.size() > 4).waitOrTimeout();
-        } finally {
-            subscriber.cancel();
-        }
+        client.connect().close();
 
-        assertThat(events).areAtLeast(1, new ExpectedClassCondition(ConnectedEvent.class));
-        assertThat(events).areAtLeast(1, new ExpectedClassCondition(ConnectionActivatedEvent.class));
-        assertThat(events).areAtLeast(1, new ExpectedClassCondition(DisconnectedEvent.class));
-        assertThat(events).areAtLeast(1, new ExpectedClassCondition(ConnectionDeactivatedEvent.class));
+        StepVerifier.create(publisher).assertNext(event -> {
+            assertThat(event.remoteAddress()).isNotNull();
+            assertThat(event.localAddress()).isNotNull();
+            assertThat(event.toString()).contains("->");
+        }).expectNextCount(3).thenCancel().verify();
 
-        ConnectionEvent event = (ConnectionEvent) events.get(0);
-        assertThat(event.remoteAddress()).isNotNull();
-        assertThat(event.localAddress()).isNotNull();
-
-        assertThat(event.toString()).contains("->");
-    }
-
-    private static class ExpectedClassCondition extends Condition<Object> {
-        private final Class<?> expectedClass;
-
-        public ExpectedClassCondition(Class<?> expectedClass) {
-            this.expectedClass = expectedClass;
-        }
-
-        @Override
-        public boolean matches(Object value) {
-            return value != null && value.getClass().equals(expectedClass);
-        }
+        client.shutdown();
     }
 }
