47aedad8c1bbf2f64654aaf9a2f61f9ce9c7d5cf
Mark Paluch <mpaluch@paluch.biz>
2017-02-19 17:17:42 +0100

    Read connect future in ReconnectHandler to local variable #465
    ReconnectHandler reads now the connect future in a local variable prior to futher interaction to avoid races in which the connect future is set to null or a different value.

--- src/main/java/com/lambdaworks/redis/protocol/ReconnectionHandler.java
+++ src/main/java/com/lambdaworks/redis/protocol/ReconnectionHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2016 the original author or authors.
+ * Copyright 2011-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,7 +72,7 @@ class ReconnectionHandler {
             long start = System.nanoTime();
 
             logger.debug("Reconnecting to Redis at {}", remoteAddress);
-            currentFuture = bootstrap.connect(remoteAddress);
+            ChannelFuture currentFuture = this.currentFuture = bootstrap.connect(remoteAddress);
             if (!currentFuture.await(timeLeft, TimeUnit.NANOSECONDS)) {
                 if (currentFuture.isCancellable()) {
                     currentFuture.cancel(true);
@@ -119,7 +119,7 @@ class ReconnectionHandler {
                 }
             }
         } finally {
-            currentFuture = null;
+            this.currentFuture = null;
         }
 
         return false;
