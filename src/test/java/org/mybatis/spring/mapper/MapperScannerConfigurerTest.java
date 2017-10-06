b8554acb4ff43a7aa8b7ed40a1bcbeed35a9a950
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-10-06 10:00:59 +0900

    Fix to close the ApplicationContext at assertion failing

--- src/test/java/org/mybatis/spring/mapper/MapperScannerConfigurerTest.java
+++ src/test/java/org/mybatis/spring/mapper/MapperScannerConfigurerTest.java
@@ -71,14 +71,16 @@ public final class MapperScannerConfigurerTest {
 
   @AfterEach
   void assertNoMapperClass() {
-    // concrete classes should always be ignored by MapperScannerPostProcessor
-    assertBeanNotLoaded("mapperClass");
+    try {
+      // concrete classes should always be ignored by MapperScannerPostProcessor
+      assertBeanNotLoaded("mapperClass");
 
-    // no method interfaces should be ignored too
-    assertBeanNotLoaded("package-info");
-    //        assertBeanNotLoaded("annotatedMapperZeroMethods"); // as of 1.1.0 mappers with no methods are loaded
-    
-    applicationContext.close();
+      // no method interfaces should be ignored too
+      assertBeanNotLoaded("package-info");
+      //        assertBeanNotLoaded("annotatedMapperZeroMethods"); // as of 1.1.0 mappers with no methods are loaded
+    } finally {
+      applicationContext.close();
+    }
   }
 
   @Test
