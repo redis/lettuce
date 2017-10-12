7f51200ace15b1ddbb0ebe4982721e18c5f99c35
Mark Paluch <mpaluch@paluch.biz>
2017-10-12 23:40:25 +0200

    Guard initialization against premature disconnect #608
    Properly propagate connect/disconnect events to prevent a race in which the disconnect is faster than setting up future chaining for event propagation.

--- src/main/java/io/lettuce/core/PlainChannelInitializer.java
+++ src/main/java/io/lettuce/core/PlainChannelInitializer.java
@@ -75,6 +75,11 @@ class PlainChannelInitializer extends io.netty.channel.ChannelInitializer<Channe
                 public void channelInactive(ChannelHandlerContext ctx) throws Exception {
 
                     clientResources.eventBus().publish(new DisconnectedEvent(local(ctx), remote(ctx)));
+
+                    if (!initializedFuture.isDone()) {
+                        initializedFuture.completeExceptionally(new RedisConnectionException("Connection closed prematurely"));
+                    }
+
                     initializedFuture = new CompletableFuture<>();
                     pingCommand = null;
                     super.channelInactive(ctx);
