6d91f626e52107ab06864f637af97e35a9fe8e4c
Mark Paluch <mpaluch@paluch.biz>
2017-12-02 14:35:44 -0800

    Make sure to write at least two bytes in RandomServerHandler #660
    Prevent hanging tests due to receiving no response.

--- src/test/java/io/lettuce/core/server/RandomServerHandler.java
+++ src/test/java/io/lettuce/core/server/RandomServerHandler.java
@@ -15,8 +15,8 @@
  */
 package io.lettuce.core.server;
 
-import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Random;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
@@ -27,21 +27,30 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  */
 public class RandomServerHandler extends ChannelInboundHandlerAdapter {
 
-    private SecureRandom random = new SecureRandom();
+    private final int count;
+
+    public RandomServerHandler() {
+
+        int count;
+        do {
+            count = new Random().nextInt(50);
+        } while (count < 1);
+
+        this.count = count;
+
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        byte initial[] = new byte[1];
-        random.nextBytes(initial);
 
-        byte[] response = new byte[Math.abs((int) initial[0])];
+        byte[] response = new byte[count];
 
         Arrays.fill(response, "A".getBytes()[0]);
 
         ByteBuf buf = ctx.alloc().heapBuffer(response.length);
 
         ByteBuf encoded = buf.writeBytes(response);
-        ctx.write(encoded);
+        ctx.writeAndFlush(encoded);
     }
 
     @Override
