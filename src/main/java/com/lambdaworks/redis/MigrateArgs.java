1f7aa1ed4077907a95447d21577e0a70610dfa7a
Mark Paluch <mpaluch@paluch.biz>
2018-04-17 10:45:19 +0200

    Polishing #733
    Extend documentation for MigrateArgs.

--- src/main/java/com/lambdaworks/redis/MigrateArgs.java
+++ src/main/java/com/lambdaworks/redis/MigrateArgs.java
@@ -25,8 +25,10 @@ import com.lambdaworks.redis.protocol.CommandKeyword;
 import com.lambdaworks.redis.protocol.CommandType;
 
 /**
- * Argument list builder for the new redis <a href="http://redis.io/commands/migrate">MIGRATE</a> command. Static import the
- * methods from {@link Builder} and chain the method calls: {@code copy().auth("foobar")}.
+ * Argument list builder for the Redis <a href="http://redis.io/commands/migrate">MIGRATE</a> command. Static import the methods
+ * from {@link Builder} and chain the method calls: {@code copy().auth("foobar")}.
+ * <p/>
+ * {@link MigrateArgs} is a mutable object and instances should be used only once to avoid shared mutable state.
  *
  * @author Mark Paluch
  */
@@ -37,6 +39,9 @@ public class MigrateArgs<K> {
     List<K> keys = new ArrayList<>();
     private char[] password;
 
+    /**
+     * Builder entry points for {@link MigrateArgs}.
+     */
     public static class Builder {
 
         /**
@@ -45,60 +50,138 @@ public class MigrateArgs<K> {
         private Builder() {
         }
 
+        /**
+         * Creates new {@link MigrateArgs} and enabling {@literal COPY}.
+         *
+         * @return new {@link MigrateArgs} with {@literal COPY} enabled.
+         * @see MigrateArgs#copy()
+         */
         public static <K> MigrateArgs<K> copy() {
             return new MigrateArgs<K>().copy();
         }
 
+        /**
+         * Creates new {@link MigrateArgs} and enabling {@literal REPLACE}.
+         *
+         * @return new {@link MigrateArgs} with {@literal REPLACE} enabled.
+         * @see MigrateArgs#replace()
+         */
         public static <K> MigrateArgs<K> replace() {
             return new MigrateArgs<K>().replace();
         }
 
+        /**
+         * Creates new {@link MigrateArgs} setting a {@code key} to migrate.
+         *
+         * @param key must not be {@literal null}.
+         * @return new {@link MigrateArgs} for {@code key} to migrate.
+         * @see MigrateArgs#key(Object)
+         */
         public static <K> MigrateArgs<K> key(K key) {
             return new MigrateArgs<K>().key(key);
         }
 
-        public static <K> MigrateArgs<K> auth(CharSequence password) {
-            return new MigrateArgs<K>().auth(password);
-        }
-
-        public static <K> MigrateArgs<K> auth(char[] password) {
-            return new MigrateArgs<K>().auth(password);
-        }
-
+        /**
+         * Creates new {@link MigrateArgs} setting {@code keys} to migrate.
+         *
+         * @param keys must not be {@literal null}.
+         * @return new {@link MigrateArgs} for {@code keys} to migrate.
+         * @see MigrateArgs#keys(Object[])
+         */
         @SafeVarargs
         public static <K> MigrateArgs<K> keys(K... keys) {
             return new MigrateArgs<K>().keys(keys);
         }
 
+        /**
+         * Creates new {@link MigrateArgs} setting {@code keys} to migrate.
+         *
+         * @param keys must not be {@literal null}.
+         * @return new {@link MigrateArgs} for {@code keys} to migrate.
+         * @see MigrateArgs#keys(Iterable)
+         */
         public static <K> MigrateArgs<K> keys(Iterable<K> keys) {
             return new MigrateArgs<K>().keys(keys);
         }
 
+        /**
+         * Creates new {@link MigrateArgs} with {@code AUTH} (target authentication) enabled.
+         *
+         * @return new {@link MigrateArgs} with {@code AUTH} (target authentication) enabled.
+         * @since 4.4.5
+         * @see MigrateArgs#auth(CharSequence)
+         */
+        public static <K> MigrateArgs<K> auth(CharSequence password) {
+            return new MigrateArgs<K>().auth(password);
+        }
+
+        /**
+         * Creates new {@link MigrateArgs} with {@code AUTH} (target authentication) enabled.
+         *
+         * @return new {@link MigrateArgs} with {@code AUTH} (target authentication) enabled.
+         * @since 4.4.5
+         * @see MigrateArgs#auth(char[])
+         */
+        public static <K> MigrateArgs<K> auth(char[] password) {
+            return new MigrateArgs<K>().auth(password);
+        }
     }
 
+    /**
+     * Do not remove the key from the local instance by setting {@code COPY}.
+     *
+     * @return {@code this} {@link MigrateArgs}.
+     */
     public MigrateArgs<K> copy() {
         this.copy = true;
         return this;
     }
 
+    /**
+     * Replace existing key on the remote instance by setting {@code REPLACE}.
+     *
+     * @return {@code this} {@link MigrateArgs}.
+     */
     public MigrateArgs<K> replace() {
         this.replace = true;
         return this;
     }
 
+    /**
+     * Migrate a single {@code key}.
+     *
+     * @param key must not be {@literal null}.
+     * @return {@code this} {@link MigrateArgs}.
+     */
     public MigrateArgs<K> key(K key) {
+
         LettuceAssert.notNull(key, "Key must not be null");
+
         this.keys.add(key);
         return this;
     }
 
+    /**
+     * Migrate one or more {@code keys}.
+     *
+     * @param keys must not be {@literal null}.
+     * @return {@code this} {@link MigrateArgs}.
+     */
     @SafeVarargs
     public final MigrateArgs<K> keys(K... keys) {
+
         LettuceAssert.notEmpty(keys, "Keys must not be empty");
+
         this.keys.addAll(Arrays.asList(keys));
         return this;
     }
 
+    /**
+     * Migrate one or more {@code keys}.
+     *
+     * @param keys must not be {@literal null}.
+     * @return {@code this} {@link MigrateArgs}.
+     */
     public MigrateArgs<K> keys(Iterable<K> keys) {
         LettuceAssert.notNull(keys, "Keys must not be null");
         for (K key : keys) {
