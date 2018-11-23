ec2899c79b782aba2fc1b6d2c9949176ad2d1900
Mark Paluch <mpaluch@paluch.biz>
2018-11-23 16:26:33 +0100

    Consider named/indexed parameters in DefaultCommandMethodVerifier #925
    Lettuce now considers declared parameters when bound by using annotations. Previously, DefaultCommandMethodVerifier counted method arguments and bind markers as individual parameters which doubled the number of command args so command validation failed.

--- src/test/java/io/lettuce/core/dynamic/DefaultCommandMethodVerifierUnitTests.java
+++ src/test/java/io/lettuce/core/dynamic/DefaultCommandMethodVerifierUnitTests.java
@@ -19,14 +19,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 
 import java.lang.reflect.Method;
-import java.util.Collections;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.lettuce.core.GeoCoordinates;
 import io.lettuce.core.KeyValue;
-import io.lettuce.core.dynamic.segment.CommandSegment;
+import io.lettuce.core.dynamic.annotation.Command;
+import io.lettuce.core.dynamic.annotation.Param;
+import io.lettuce.core.dynamic.segment.AnnotationCommandSegmentFactory;
+import io.lettuce.core.dynamic.segment.CommandSegmentFactory;
 import io.lettuce.core.dynamic.segment.CommandSegments;
 import io.lettuce.core.dynamic.support.ReflectionUtils;
 import io.lettuce.core.internal.LettuceLists;
@@ -45,10 +47,11 @@ class DefaultCommandMethodVerifierUnitTests {
         CommandDetail mget = new CommandDetail("mget", -2, null, 0, 0, 0);
         CommandDetail randomkey = new CommandDetail("randomkey", 1, null, 0, 0, 0);
         CommandDetail rpop = new CommandDetail("rpop", 2, null, 0, 0, 0);
+        CommandDetail lpop = new CommandDetail("lpop", 2, null, 0, 0, 0);
         CommandDetail set = new CommandDetail("set", 3, null, 0, 0, 0);
         CommandDetail geoadd = new CommandDetail("geoadd", -4, null, 0, 0, 0);
 
-        sut = new DefaultCommandMethodVerifier(LettuceLists.newList(mget, randomkey, rpop, set, geoadd));
+        sut = new DefaultCommandMethodVerifier(LettuceLists.newList(mget, randomkey, rpop, lpop, set, geoadd));
     }
 
     @Test
@@ -58,7 +61,7 @@ class DefaultCommandMethodVerifierUnitTests {
             validateMethod("megt");
             fail("Missing CommandMethodSyntaxException");
         } catch (CommandMethodSyntaxException e) {
-            assertThat(e).hasMessageContaining("Command megt does not exist. Did you mean: MGET, SET?");
+            assertThat(e).hasMessageContaining("Command MEGT does not exist. Did you mean: MGET, SET?");
         }
     }
 
@@ -77,9 +80,10 @@ class DefaultCommandMethodVerifierUnitTests {
     @Test
     void shouldPassWithCorrectParameterCount() {
 
+        validateMethod("lpop", String.class);
+        validateMethod("rpop", String.class);
         validateMethod("mget", String.class);
         validateMethod("randomkey");
-        validateMethod("rpop", String.class);
         validateMethod("set", KeyValue.class);
         validateMethod("geoadd", String.class, String.class, GeoCoordinates.class);
     }
@@ -109,9 +113,11 @@ class DefaultCommandMethodVerifierUnitTests {
     private void validateMethod(String methodName, Class<?>... parameterTypes) {
 
         Method method = ReflectionUtils.findMethod(MyInterface.class, methodName, parameterTypes);
+        CommandSegmentFactory commandSegmentFactory = new AnnotationCommandSegmentFactory();
         CommandMethod commandMethod = DeclaredCommandMethod.create(method);
+        CommandSegments commandSegments = commandSegmentFactory.createCommandSegments(commandMethod);
 
-        sut.validate(new CommandSegments(Collections.singletonList(CommandSegment.constant(methodName))), commandMethod);
+        sut.validate(commandSegments, commandMethod);
     }
 
     private static interface MyInterface {
@@ -132,7 +138,11 @@ class DefaultCommandMethodVerifierUnitTests {
 
         void randomkey(String key);
 
+        @Command("RPOP ?0")
         void rpop(String key);
+
+        @Command("LPOP :key")
+        void lpop(@Param("key") String key);
 
         void rpop(String key1, String key2);
     }
