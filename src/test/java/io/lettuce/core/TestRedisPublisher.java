e0e83b09bfe5a355c2224946e6f4078f18390140
Mark Paluch <mpaluch@paluch.biz>
2018-12-03 14:31:46 +0100

    Allow usage of publishOn scheduler for reactive signal emission #905
    Emission of data and completion signals using the reactive API can now use Lettuce's EventExecutorGroup instead of the I/O thread. Scheduler usage allows multiplexing of threads and early I/O thread release.
    
    Using a single Redis connection in a reactive context can cause effectively single-threaded behavior as processing of downstream operators happens typically on the I/O thread. Compute-intensive processing consumes I/O thread capacity and results in performance decrease.

--- src/test/java/io/lettuce/core/TestRedisPublisher.java
+++ src/test/java/io/lettuce/core/TestRedisPublisher.java
@@ -19,6 +19,7 @@ import java.util.function.Supplier;
 
 import io.lettuce.core.api.StatefulConnection;
 import io.lettuce.core.protocol.RedisCommand;
+import io.netty.util.concurrent.ImmediateEventExecutor;
 
 /**
  * @author Mark Paluch
@@ -26,11 +27,11 @@ import io.lettuce.core.protocol.RedisCommand;
 public class TestRedisPublisher<K, V, T> extends RedisPublisher<K, V, T> {
 
     public TestRedisPublisher(RedisCommand<K, V, T> staticCommand, StatefulConnection<K, V> connection, boolean dissolve) {
-        super(staticCommand, connection, dissolve);
+        super(staticCommand, connection, dissolve, ImmediateEventExecutor.INSTANCE);
     }
 
     public TestRedisPublisher(Supplier<RedisCommand<K, V, T>> redisCommandSupplier, StatefulConnection<K, V> connection,
             boolean dissolve) {
-        super(redisCommandSupplier, connection, dissolve);
+        super(redisCommandSupplier, connection, dissolve, ImmediateEventExecutor.INSTANCE);
     }
 }
