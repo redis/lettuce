df5b7f439ed97a4d601d0457e0c904ff66e67160
Kazuki Shimizu <kazuki43zoo@gmail.com>
2019-01-20 22:21:08 +0900

    Fix some sonar reports

--- src/main/java/org/mybatis/logging/LoggerFactory.java
+++ src/main/java/org/mybatis/logging/LoggerFactory.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2010-2017 the original author or authors.
+ *    Copyright 2010-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -23,6 +23,10 @@ import org.apache.ibatis.logging.LogFactory;
  * @author Putthiphong Boonphong
  */
 public class LoggerFactory {
+
+  private LoggerFactory() {
+    // NOP
+  }
 
   public static Logger getLogger(Class<?> aClass) {
     return new Logger(LogFactory.getLog(aClass));
--- src/main/java/org/mybatis/spring/MyBatisSystemException.java
+++ src/main/java/org/mybatis/spring/MyBatisSystemException.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2010-2016 the original author or authors.
+ *    Copyright 2010-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.springframework.dao.UncategorizedDataAccessException;
  *
  * @author Hunter Presnall
  */
+@SuppressWarnings("squid:MaximumInheritanceDepth") // It is the intended design
 public class MyBatisSystemException extends UncategorizedDataAccessException {
 
   private static final long serialVersionUID = -5284728621670758939L;
