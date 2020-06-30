b028fd4f6058a829a206388d7c3d1170d6280dd2
Mark Paluch <mpaluch@paluch.biz>
2020-06-30 15:34:22 +0200

    Polishing
    Await condition completion.

--- src/test/java/io/lettuce/core/protocol/ConnectionFailureIntegrationTests.java
+++ src/test/java/io/lettuce/core/protocol/ConnectionFailureIntegrationTests.java
@@ -318,6 +318,7 @@ class ConnectionFailureIntegrationTests extends TestSupport {
             client.connect(redisUri);
             fail("Missing Exception");
         } catch (Exception e) {
+            Wait.untilEquals(false, ref.get()::isRegistered).waitOrTimeout();
             assertThat(ref.get().isOpen()).isFalse();
             assertThat(ref.get().isRegistered()).isFalse();
         } finally {
