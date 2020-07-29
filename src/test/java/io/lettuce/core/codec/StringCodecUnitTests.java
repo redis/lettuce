094e3924e43190703dea6ca6adeb67f341e04ea4
Dimitris Mandalidis <dimitris.mandalidis@gmail.com>
2020-07-29 16:08:18 +0300

    Commonize string byte count calculations

--- src/test/java/io/lettuce/core/codec/StringCodecUnitTests.java
+++ src/test/java/io/lettuce/core/codec/StringCodecUnitTests.java
@@ -18,12 +18,14 @@ package io.lettuce.core.codec;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
 
 /**
  * @author Mark Paluch
@@ -115,5 +117,15 @@ class StringCodecUnitTests {
                 .isEqualTo((int) (teststring.length() * 1.1));
         assertThat(new StringCodec(StandardCharsets.US_ASCII).estimateSize(teststring)).isEqualTo(teststring.length());
         assertThat(new StringCodec(StandardCharsets.ISO_8859_1).estimateSize(teststring)).isEqualTo(teststring.length());
+    }
+    
+    @Test
+    public void calculateStringSize() {
+        assertThat(new StringCodec(StandardCharsets.UTF_8).calculateStringBytes(teststring, false))
+            .isEqualTo(teststring.length() * 3);
+        assertThat(new StringCodec(StandardCharsets.US_ASCII).calculateStringBytes(teststring, false))
+            .isEqualTo(teststring.length());
+        assertThat(new StringCodec(StandardCharsets.ISO_8859_1).calculateStringBytes(teststring, false))
+            .isEqualTo(teststring.length());
     }
 }
