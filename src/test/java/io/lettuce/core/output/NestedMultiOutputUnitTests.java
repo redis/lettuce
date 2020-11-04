fe70d44ba8cc3670c8b34658f70ecc9fec9d5345
Julien Ruaux <jruaux@users.noreply.github.com>
2020-11-04 15:23:25 -0800

    Implement set(double) in NestedMultiOutput #1486
    NestedMultiOutput does not implement CommandOutput.set(double number) which causes problem with RediSearch FT.INFO command

--- src/test/java/io/lettuce/core/output/NestedMultiOutputUnitTests.java
+++ src/test/java/io/lettuce/core/output/NestedMultiOutputUnitTests.java
@@ -35,4 +35,15 @@ class NestedMultiOutputUnitTests {
         output.setError(StandardCharsets.US_ASCII.encode("Oops!"));
         assertThat(output.getError()).isNotNull();
     }
+
+    @Test
+    void nestedMultiDouble() {
+        NestedMultiOutput<String, String> output = new NestedMultiOutput<>(StringCodec.UTF8);
+        double value = 123.456;
+        output.set(value);
+        assertThat(output.get()).isNotNull();
+        assertThat(output.get()).size().isEqualTo(1);
+        assertThat(output.get().get(0)).isEqualTo(value);
+    }
+
 }
