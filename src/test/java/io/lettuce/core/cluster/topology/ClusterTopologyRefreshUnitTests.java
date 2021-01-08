ee3e70f613074531aef9874a01bbbeb72dbd2cbb
Mark Paluch <mpaluch@paluch.biz>
2021-01-08 14:29:26 +0100

    Update copyright years to 2021 #1573

--- src/test/java/io/lettuce/core/cluster/topology/ClusterTopologyRefreshUnitTests.java
+++ src/test/java/io/lettuce/core/cluster/topology/ClusterTopologyRefreshUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 the original author or authors.
+ * Copyright 2011-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,7 +58,7 @@ import io.netty.util.concurrent.EventExecutorGroup;
 
 /**
  * Unit tests for {@link ClusterTopologyRefresh}.
- * 
+ *
  * @author Mark Paluch
  * @author Christian Weitendorf
  */
