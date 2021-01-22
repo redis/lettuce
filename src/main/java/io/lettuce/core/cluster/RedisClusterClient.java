230d7baa21e2edcbc3910cd1e8ca88f0944e72a0
Mark Paluch <mpaluch@paluch.biz>
2021-01-22 13:50:00 +0100

    Introduce template method to supply a custom ClusterTopologyRefresh implementation #1598

--- src/main/java/io/lettuce/core/cluster/RedisClusterClient.java
+++ src/main/java/io/lettuce/core/cluster/RedisClusterClient.java
@@ -141,8 +141,7 @@ public class RedisClusterClient extends AbstractRedisClient {
 
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(RedisClusterClient.class);
 
-    private final ClusterTopologyRefresh refresh = ClusterTopologyRefresh.create(new NodeConnectionFactoryImpl(),
-            getResources());
+    private final ClusterTopologyRefresh refresh;
 
     private final ClusterTopologyRefreshScheduler topologyRefreshScheduler = new ClusterTopologyRefreshScheduler(
             this::getClusterClientOptions, this::getPartitions, this::refreshPartitionsAsync, getResources());
@@ -158,7 +157,8 @@ public class RedisClusterClient extends AbstractRedisClient {
 
         super(null);
 
-        initialUris = Collections.emptyList();
+        this.initialUris = Collections.emptyList();
+        this.refresh = createTopologyRefresh();
     }
 
     /**
@@ -178,6 +178,7 @@ public class RedisClusterClient extends AbstractRedisClient {
         assertSameOptions(redisURIs);
 
         this.initialUris = Collections.unmodifiableList(LettuceLists.newList(redisURIs));
+        this.refresh = createTopologyRefresh();
 
         setDefaultTimeout(getFirstUri().getTimeout());
         setOptions(ClusterClientOptions.create());
@@ -1087,6 +1088,16 @@ public class RedisClusterClient extends AbstractRedisClient {
                 function.accept((T) c);
             }
         }
+    }
+
+    /**
+     * Template method to create {@link ClusterTopologyRefresh}. Can be overriden by subclasses.
+     *
+     * @return
+     * @since 6.0.3
+     */
+    protected ClusterTopologyRefresh createTopologyRefresh() {
+        return ClusterTopologyRefresh.create(new NodeConnectionFactoryImpl(), getResources());
     }
 
     /**
