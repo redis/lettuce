0f885dd01732d8e7c29de2e73e1592960f2dd0eb
Per Lundberg <per.lundberg@hibox.tv>
2021-04-07 10:05:33 +0300

    Improve deprecation notice in RedisURI.withPassword() #1707

--- src/main/java/io/lettuce/core/RedisURI.java
+++ src/main/java/io/lettuce/core/RedisURI.java
@@ -1467,9 +1467,14 @@ public class RedisURI implements Serializable, ConnectionPoint {
         /**
          * Configures authentication.
          *
+         * This method is deprecated as of Lettuce 6.0. The reason is that {@link String} has a
+         * strong caching affinity and the JVM cannot easily GC {@code String} instances. Therefore
+         * we suggest using either {@code char[]} or a custom {@link CharSequence} (e.g. {@link
+         * StringBuilder} or netty's {@link io.netty.util.AsciiString}).
+         *
          * @param password the password
          * @return the builder
-         * @deprecated since 6.0. Use {@link #withPassword(CharSequence)} or {@link #withPassword(char[])} avoid String caching.
+         * @deprecated since 6.0. Use {@link #withPassword(CharSequence)} or {@link #withPassword(char[])} to avoid String caching.
          */
         @Deprecated
         public Builder withPassword(String password) {
