cacdfc670a8d438a413b5ecf299196780ad41c2f
Mark Paluch <mpaluch@paluch.biz>
2021-06-09 09:43:45 +0200

    Introduce system property to disable JFR support #1767

--- src/main/java/io/lettuce/core/event/jfr/EventRecorderHolder.java
+++ src/main/java/io/lettuce/core/event/jfr/EventRecorderHolder.java
@@ -16,6 +16,9 @@
 package io.lettuce.core.event.jfr;
 
 import io.lettuce.core.internal.LettuceClassUtils;
+import io.netty.util.internal.SystemPropertyUtil;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 
 /**
  * Holder for {@link EventRecorder}.
@@ -25,8 +28,34 @@ import io.lettuce.core.internal.LettuceClassUtils;
  */
 class EventRecorderHolder {
 
-    private static final boolean JFR_AVAILABLE = LettuceClassUtils.isPresent("jdk.jfr.Event");
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(EventRecorderHolder.class);
 
-    static final EventRecorder EVENT_RECORDER = JFR_AVAILABLE ? new JfrEventRecorder() : NoOpEventRecorder.INSTANCE;
+    private static final String JFR_ENABLED_KEY = "io.lettuce.core.jfr";
+
+    private static final boolean JFR_ENABLED = Boolean.parseBoolean(SystemPropertyUtil.get(JFR_ENABLED_KEY, "true"));
+
+    static final EventRecorder EVENT_RECORDER;
+
+    static {
+
+        boolean available = LettuceClassUtils.isPresent("jdk.jfr.Event");
+        EventRecorder eventRecorder = NoOpEventRecorder.INSTANCE;
+
+        if (available) {
+
+            if (JFR_ENABLED) {
+                logger.debug("Starting with JFR support");
+                eventRecorder = new JfrEventRecorder();
+            } else {
+                logger.debug(
+                        String.format("Starting without optional JFR support. JFR use is disabled via System properties (%s)",
+                                JFR_ENABLED_KEY));
+            }
+        } else {
+            logger.debug("Starting without optional JFR support. JFR unavailable.");
+        }
+
+        EVENT_RECORDER = eventRecorder;
+    }
 
 }
