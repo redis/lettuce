e966a4e8215e78bd27ee752cac4c08a54478e625
Rohan Nagar <rohan.nagar@utexas.edu>
2021-09-08 10:20:39 -0700

    Maintain argument order in AclSetuserArgs #1847
    Original pull request: #1848.

--- src/test/java/io/lettuce/core/AclSetuserArgsUnitTests.java
+++ src/test/java/io/lettuce/core/AclSetuserArgsUnitTests.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import io.lettuce.core.codec.StringCodec;
 import io.lettuce.core.protocol.CommandArgs;
+import io.lettuce.core.protocol.CommandType;
 
 /**
  * Unit tests for {@link AclSetuserArgs}.
@@ -31,6 +32,49 @@ import io.lettuce.core.protocol.CommandArgs;
 class AclSetuserArgsUnitTests {
 
     @Test
+    void shouldMaintainCommandOrder() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.off()
+            .addCommand(CommandType.HELLO)
+            .removeCommand(CommandType.ACL)
+            .addCommand(CommandType.PING);
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("OFF +HELLO -ACL +PING");
+    }
+
+    @Test
+    void shouldAllowAdditionalCommandsWithReset() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.reset().on().addCategory(AclCategory.ADMIN);
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("RESET ON +@ADMIN");
+    }
+
+    @Test
+    void shouldNotAlwaysSetActiveStatus() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.addCategory(AclCategory.ADMIN);
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("+@ADMIN");
+    }
+
+    @Test
+    void shouldAddAllCommands() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().allCommands();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON ALLCOMMANDS");
+    }
+
+    @Test
     void shouldRemoveAllCommands() {
 
         AclSetuserArgs args = AclSetuserArgs.Builder.on().noCommands();
@@ -38,6 +82,126 @@ class AclSetuserArgsUnitTests {
         args.build(commandArgs);
 
         assertThat(commandArgs.toCommandString()).isEqualTo("ON NOCOMMANDS");
+    }
+
+    @Test
+    void shouldAddAllKeys() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().allKeys();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON ALLKEYS");
+    }
+
+    @Test
+    void shouldResetKeys() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().resetKeys();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON RESETKEYS");
+    }
+
+    @Test
+    void shouldSetKeyPattern() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().keyPattern("*");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON ~*");
+    }
+
+    @Test
+    void shouldAddAllChannels() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().allChannels();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON ALLCHANNELS");
+    }
+
+    @Test
+    void shouldResetChannels() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().resetChannels();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON RESETCHANNELS");
+    }
+
+    @Test
+    void shouldSetChannelPattern() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().channelPattern("*");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON &*");
+    }
+
+    @Test
+    void shouldAddPassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().addPassword("password");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON >password");
+    }
+
+    @Test
+    void shouldAddHashedPassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().addHashedPassword("password");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON #password");
+    }
+
+    @Test
+    void shouldRemovePassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().removePassword("password");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON <password");
+    }
+
+    @Test
+    void shouldRemoveHashedPassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().removeHashedPassword("password");
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON !password");
+    }
+
+    @Test
+    void shouldSetNoPassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().nopass();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON NOPASS");
+    }
+
+    @Test
+    void shouldResetPassword() {
+
+        AclSetuserArgs args = AclSetuserArgs.Builder.on().resetpass();
+        CommandArgs<String, String> commandArgs = new CommandArgs<>(StringCodec.UTF8);
+        args.build(commandArgs);
+
+        assertThat(commandArgs.toCommandString()).isEqualTo("ON RESETPASS");
     }
 
     @Test
