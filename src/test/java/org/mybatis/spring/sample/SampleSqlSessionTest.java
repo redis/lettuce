d520290b9869b3dde2328a6221f885f03238f49d
Jeremy Landis <jeremylandis@hotmail.com>
2023-04-06 21:13:41 -0400

    Revert "[test] Remove dirties context annotation"
    This reverts commit 97904b5729feb7fe2394031a40e57a8ab5f099e1.

--- src/test/java/org/mybatis/spring/sample/SampleSqlSessionTest.java
+++ src/test/java/org/mybatis/spring/sample/SampleSqlSessionTest.java
@@ -21,12 +21,14 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.sample.domain.User;
 import org.mybatis.spring.sample.service.BarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * Example of basic MyBatis-Spring integration usage with a manual DAO implementation that subclasses
  * SqlSessionDaoSupport.
  */
+@DirtiesContext
 @SpringJUnitConfig(locations = { "classpath:org/mybatis/spring/sample/config/applicationContext-sqlsession.xml" })
 class SampleSqlSessionTest {
 
