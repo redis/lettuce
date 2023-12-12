512f022940bf49d6758ab66e5169b0289ea8242c
kajte <33686434+kajte@users.noreply.github.com>
2023-12-13 00:06:35 +0200

    Add GEOSEARCH to read-only commands #2568
    Set GEOSEARCH as read-only command to allow running the command on replicas.
    
    Original pull request: #2569

--- src/test/java/io/lettuce/core/cluster/ClusterReadOnlyCommandsUnitTests.java
+++ src/test/java/io/lettuce/core/cluster/ClusterReadOnlyCommandsUnitTests.java
@@ -31,7 +31,7 @@ class ClusterReadOnlyCommandsUnitTests {
 
     @Test
     void testCount() {
-        assertThat(ClusterReadOnlyCommands.getReadOnlyCommands()).hasSize(83);
+        assertThat(ClusterReadOnlyCommands.getReadOnlyCommands()).hasSize(84);
     }
 
     @Test
