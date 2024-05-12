4fe8d21f066abe8c3d64ddf478058b81b2520f61
sojung.kim <thwjd2717@gmail.com>
2024-05-13 00:49:01 +0900

    test(#2341): Add test cases for slot initialization in RedisClusterNode constructors

--- src/test/java/io/lettuce/core/cluster/models/partitions/RedisClusterNodeUnitTests.java
+++ src/test/java/io/lettuce/core/cluster/models/partitions/RedisClusterNodeUnitTests.java
@@ -3,6 +3,8 @@ package io.lettuce.core.cluster.models.partitions;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +17,55 @@ import io.lettuce.core.cluster.SlotHash;
  * @author Mark Paluch
  */
 class RedisClusterNodeUnitTests {
+
+    @Test
+    void shouldCreateNodeWithEmptySlots() {
+
+        BitSet slots = new BitSet();
+        RedisClusterNode node = new RedisClusterNode(RedisURI.create("localhost", 6379), "1", true, null, 0, 0, 0, slots,
+                Collections.emptySet());
+        assertThat(node.getSlots()).isEmpty();
+        assertThat(node.getSlots()).isNotNull();
+    }
+
+    @Test
+    void shouldCreateNodeWithNonEmptySlots() {
+
+        BitSet slots = new BitSet();
+        slots.set(1);
+        slots.set(2);
+        RedisClusterNode node = new RedisClusterNode(RedisURI.create("localhost", 6379), "1", true, null, 0, 0, 0, slots,
+                Collections.emptySet());
+
+        assertThat(node.getSlots()).containsExactly(1, 2);
+    }
+
+    @Test
+    void shouldCopyNodeWithEmptySlots() {
+
+        BitSet slots = new BitSet();
+        RedisClusterNode originalNode = new RedisClusterNode(RedisURI.create("localhost", 6379), "1", true, null, 0, 0, 0,
+                slots, Collections.emptySet());
+
+        RedisClusterNode copiedNode = new RedisClusterNode(originalNode);
+
+        assertThat(copiedNode.getSlots()).isEmpty();
+        assertThat(copiedNode.getSlots()).isNotNull();
+    }
+
+    @Test
+    void shouldCopyNodeWithNonEmptySlots() {
+
+        BitSet slots = new BitSet();
+        slots.set(1);
+        slots.set(2);
+        RedisClusterNode originalNode = new RedisClusterNode(RedisURI.create("localhost", 6379), "1", true, null, 0, 0, 0,
+                slots, Collections.emptySet());
+
+        RedisClusterNode copiedNode = new RedisClusterNode(originalNode);
+
+        assertThat(copiedNode.getSlots()).containsExactly(1, 2);
+    }
 
     @Test
     void shouldCopyNode() {
