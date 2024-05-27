1e21d42de13da81aa50cac11affc352fe6a3e99e
Tihomir Mateev <tihomir.mateev@gmail.com>
2024-05-27 19:11:03 +0300

    Submitted one more file by mistake

--- src/test/java/io/lettuce/core/SocketOptionsIntegrationTests.java
+++ src/test/java/io/lettuce/core/SocketOptionsIntegrationTests.java
@@ -1,16 +1,18 @@
 package io.lettuce.core;
 
-import io.lettuce.test.LettuceExtension;
-import io.netty.channel.ConnectTimeoutException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
-import javax.inject.Inject;
 import java.net.SocketException;
 import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.lettuce.test.LettuceExtension;
+import io.netty.channel.ConnectTimeoutException;
 
 /**
  * @author Mark Paluch
@@ -48,29 +50,5 @@ class SocketOptionsIntegrationTests extends TestSupport {
             }
         }
     }
-
-//    @Test
-//    void testConnectTimeout() {
-//
-//        SocketOptions socketOptions = SocketOptions.builder().connectTimeout(100, TimeUnit.MILLISECONDS).build();
-//        client.setOptions(ClientOptions.builder().socketOptions(socketOptions).build());
-//
-//        try {
-//            client.connect(RedisURI.create("2:4:5:5::1", 60000));
-//            fail("Missing RedisConnectionException");
-//        } catch (RedisConnectionException e) {
-//
-//            if (e.getCause() instanceof ConnectTimeoutException) {
-//                assertThat(e).hasRootCauseInstanceOf(ConnectTimeoutException.class);
-//                assertThat(e.getCause()).hasMessageContaining("connection timed out");
-//                return;
-//            }
-//
-//            if (e.getCause() instanceof SocketException) {
-//                // Network is unreachable or No route to host are OK as well.
-//                return;
-//            }
-//        }
-//    }
 
 }
