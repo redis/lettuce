c5a2975778e67679bd5e52cfb643ac6bf940a463
jinkshower <askthem@naver.com>
2024-09-16 21:44:25 +0900

    Add defensive copy for Futures allOf() method (#2943)
    * Add defensive copy
    
    Closes #2935
    
    * Polishing
    
    * Forgot to call formatter, shame on me
    
    ---------
    
    Co-authored-by: Tihomir Mateev <tihomir.mateev@gmail.com>

--- src/test/java/io/lettuce/core/internal/FuturesUnitTests.java
+++ src/test/java/io/lettuce/core/internal/FuturesUnitTests.java
@@ -4,7 +4,13 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +22,7 @@ import io.lettuce.core.RedisCommandInterruptedException;
  * Unit tests for {@link Futures}.
  *
  * @author Mark Paluch
+ * @author Tihomir Mateev
  */
 class FuturesUnitTests {
 
@@ -54,6 +61,35 @@ class FuturesUnitTests {
         }
 
         assertThat(Thread.currentThread().isInterrupted()).isTrue();
+    }
+
+    @Test
+    void allOfShouldNotThrow() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Throwable> issues = new ArrayList<>();
+        List<CompletableFuture<Void>> futures = Collections.synchronizedList(new ArrayList<>());
+        // Submit multiple threads to perform concurrent operations
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    for (int y = 0; y < 1000; y++) {
+                        futures.add(new CompletableFuture<>());
+                    }
+
+                    Futures.allOf(futures);
+                } catch (Exception e) {
+                    issues.add(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        // wait for all threads to complete
+        latch.await();
+        assertThat(issues).doesNotHaveAnyElementsOfTypes(ArrayIndexOutOfBoundsException.class);
     }
 
 }
