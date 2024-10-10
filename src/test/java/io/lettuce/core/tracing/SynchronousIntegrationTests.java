c1d74de358d0c61b1a5957136042a1001b092d4b
Thach Le <thach.le@linecorp.com>
2024-10-10 17:30:43 +0700

    Polishing

--- src/test/java/io/lettuce/core/tracing/SynchronousIntegrationTests.java
+++ src/test/java/io/lettuce/core/tracing/SynchronousIntegrationTests.java
@@ -77,7 +77,7 @@ public class SynchronousIntegrationTests extends SampleTestRunner {
                 assertThat(finishedSpan.getTags()).containsKeys("db.operation");
                 assertThat(finishedSpan.getTags()).containsKeys("server.address");
                 assertThat(finishedSpan.getTags()).containsKeys("db.namespace");
-                assertThat(finishedSpan.getTags()).containsKeys("db.user");
+                assertThat(finishedSpan.getTags()).containsKeys("user.name");
             }
 
             assertThat(commands).extracting(RedisCommand::getType).contains(CommandType.PING, CommandType.HELLO);
