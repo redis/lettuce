c1826e57c04c3f3adc7b8849987af67377ee1ae3
ggivo <ivo.gaydazhiev@redis.com>
2025-01-15 17:17:15 +0200

    formating

--- src/test/java/io/lettuce/core/RedisContainerIntegrationTests.java
+++ src/test/java/io/lettuce/core/RedisContainerIntegrationTests.java
@@ -31,15 +31,11 @@ public class RedisContainerIntegrationTests {
     private static Exception initializationException;
 
     public static ComposeContainer CLUSTERED_STACK = new ComposeContainer(
-            new File("src/test/resources/docker/docker-compose.yml"))
-            .withExposedService(REDIS_STACK_CLUSTER, 36379)
-            .withExposedService(REDIS_STACK_CLUSTER, 36380)
-            .withExposedService(REDIS_STACK_CLUSTER, 36381)
-            .withExposedService(REDIS_STACK_STANDALONE, 6379)
-            .withEnv("CLIENT_LIBS_TEST_IMAGE", "redislabs/client-libs-test")
-            .withEnv("REDIS_STACK_VERSION", REDIS_STACK_VERSION)
-            .withPull(false)
-            .withLocalCompose(true);
+            new File("src/test/resources/docker/docker-compose.yml")).withExposedService(REDIS_STACK_CLUSTER, 36379)
+                    .withExposedService(REDIS_STACK_CLUSTER, 36380).withExposedService(REDIS_STACK_CLUSTER, 36381)
+                    .withExposedService(REDIS_STACK_STANDALONE, 6379)
+                    .withEnv("CLIENT_LIBS_TEST_IMAGE", "redislabs/client-libs-test")
+                    .withEnv("REDIS_STACK_VERSION", REDIS_STACK_VERSION).withPull(false).withLocalCompose(true);
 
     // Singleton container pattern - start the containers only once
     // See https://java.testcontainers.org/test_framework_integration/manual_lifecycle_control/#singleton-containers
@@ -50,8 +46,7 @@ public class RedisContainerIntegrationTests {
         CLUSTERED_STACK.withLogConsumer(REDIS_STACK_CLUSTER, (OutputFrame frame) -> LOGGER.debug(frame.getUtf8String()));
         CLUSTERED_STACK.withLogConsumer(REDIS_STACK_STANDALONE, (OutputFrame frame) -> LOGGER.debug(frame.getUtf8String()));
 
-        CLUSTERED_STACK.waitingFor(REDIS_STACK_CLUSTER,
-                Wait.forLogMessage("Cluster created with nodes:.*", 1));
+        CLUSTERED_STACK.waitingFor(REDIS_STACK_CLUSTER, Wait.forLogMessage("Cluster created with nodes:.*", 1));
         do {
             try {
                 CLUSTERED_STACK.start();
