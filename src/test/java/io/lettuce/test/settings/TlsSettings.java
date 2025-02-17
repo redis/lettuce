d3848d15e4fe7a786338a730cd4d178c0303fc47
kiryazovi-redis <ivaylo.kiryazov@redis.com>
2025-02-17 10:03:10 +0200

    Migrate Lettuce test setup to use client-lib-test (#3158)
    * add initial skeleton without stunnel
    
    * fix up some paths
    
    * fix the standalone ones.
    
    * fix issues which crash the containers.
    
    * add more file renames.
    
    * add missed configs
    
    * Fix up the docker-compose. TODO: actual testing, after being compliant
    
    * fix sentinel to work
    
    * fix 70 out of 80 failing tests by fixing configuration
    
    * fix make file, this should be committed
    
    * fix up more tests, now only ssl stuff seems to remain. (4 tests)
    
    * fix up one tls, but we need to fix up the java code for the store
    
    * fix some of the ssl tests
    
    * reduce test failures to 3
    
    * fix java
    
    * fix the last of ssl tests. TODO: retest the key tests
    
    * add docker to the makefile
    
    * add fixes for several tests
    
    * formatting
    
    * attempt to create keystore file via java
    
    * format
    
    * Revert "format"
    
    This reverts commit 79c2171df3776042e39b5e48180b90babbd90ab3.
    
    * Revert "attempt to create keystore file via java"
    
    This reverts commit c4e3b08ad594874900a7c56da6bbb28b6d89e6e5.
    
    * ad some path fixes
    
    * Fix standaloneWithClientCertificates
       - configure keystore
       - boostrap a redis server requiring tls-auth-clients
    
    * Remove some of old makefile work + fix up paths so they are not destroyed by mvn clean
    
    * attempt integration.yml to work with docker
    
    * experiment with version
    
    * fix clean-up-step
    
    * ordering
    
    * fix up move versioning and try to debug folder+unix issue
    
    * try a sock solution proposed by Ivo
    
    * fix up supported version
    
    * possible fix
    
    * make file name shorte
    
    * fix up paths, yet again, idk why there was docker
    
    * test out new waiting strategy
    
    * revet old waiting
    
    * fix default version and disable a test with server regression
    
    * "Fix" test. Problem is that acldeluser doesn't delete previous Japanese user.
    
    * further fix up acl, and later will create a PR for it
    
    * fix up benchmarks + fix up integration sudo and makefile.
    
    * format
    
    * add some readme fixes
    
    * fix docker compose down
    
    * fix up benchmarks
    
    * remove useless files
    
    * Revert ""Fix" test. Problem is that acldeluser doesn't delete previous Japanese user."
    
    This reverts commit f30a3d6c0be2c2bdf876650f802073611af5cf99.
    
    * Revert "further fix up acl, and later will create a PR for it"
    
    This reverts commit 1aad1a82db4975051613bb7f54f1cfb171b4394f.
    
    * Revert ""Fix" test. Problem is that acldeluser doesn't delete previous Japanese user."
    
    This reverts commit f30a3d6c0be2c2bdf876650f802073611af5cf99.
    
    * "Fix" test. Problem is that acldeluser doesn't delete previous Japanese user.
    
    * fix up merge issues
    
    * fix more merge issues
    
    * fix addjirahere thing
    
    * fix review issues
    
    ---------
    
    Co-authored-by: ggivo <ivo.gaydazhiev@redis.com>

---
+++ src/test/java/io/lettuce/test/settings/TlsSettings.java
@@ -0,0 +1,133 @@
+package io.lettuce.test.settings;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TlsSettings {
+
+    private static final String TRUST_STORE_TYPE = "PKCS12";
+
+    private static final String TEST_WORK_FOLDER = System.getenv().getOrDefault("TEST_WORK_FOLDER", "work/docker");
+
+    private static final String TEST_SERVER_CERT = "redis.crt";
+
+    private static final String TEST_CLIENT_P12 = "client.p12";
+
+    private static final String TEST_CLIENT_CERT = "client.crt";
+
+    private static final String TEST_CLIENT_KEY = "client.key";
+
+    private static final String TEST_CA_CERT = "ca.crt";
+
+    private static final String TEST_TRUSTSTORE = "truststore.jks";
+
+    public static Path envClientP12(Path certLocation) {
+        return Paths.get(TEST_WORK_FOLDER, certLocation.toString(), TEST_CLIENT_P12);
+    }
+
+    public static Path envServerCert(Path certLocation) {
+        return Paths.get(TEST_WORK_FOLDER, certLocation.toString(), TEST_SERVER_CERT);
+    }
+
+    public static Path envCa(Path certLocation) {
+        return Paths.get(TEST_WORK_FOLDER, certLocation.toString(), TEST_CA_CERT);
+    }
+
+    public static Path testTruststorePath(String name) {
+        return Paths.get(TEST_WORK_FOLDER, name + '-' + TEST_TRUSTSTORE);
+    }
+
+    /**
+     * Creates an empty truststore.
+     *
+     * @return An empty KeyStore object.
+     * @throws KeyStoreException If there's an error initializing the truststore.
+     * @throws IOException If there's an error loading the truststore.
+     * @throws NoSuchAlgorithmException If the algorithm used to check the integrity of the truststore cannot be found.
+     * @throws CertificateException If any of the certificates in the truststore could not be loaded.
+     */
+    private static KeyStore createTruststore()
+            throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException {
+        KeyStore trustStore = KeyStore.getInstance(TRUST_STORE_TYPE);
+        trustStore.load(null, null);
+        return trustStore;
+    }
+
+    /**
+     * Loads an X.509 certificate from the given file path.
+     *
+     * @param certPath Path to the certificate file.
+     * @return An X509Certificate object.
+     * @throws Exception If there's an error loading the certificate.
+     */
+    private static X509Certificate loadCertificate(Path certPath) throws Exception {
+        try (FileInputStream fis = new FileInputStream(certPath.toFile())) {
+            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+            return (X509Certificate) certFactory.generateCertificate(fis);
+        }
+    }
+
+    /**
+     * Adds a trusted certificate to the given truststore.
+     *
+     * @param trustStore The KeyStore object.
+     * @param alias Alias for the certificate.
+     * @param certPath Path to the certificate file.
+     * @throws Exception If there's an error adding the certificate.
+     */
+    private static void addTrustedCertificate(KeyStore trustStore, String alias, Path certPath) throws Exception {
+        X509Certificate cert = loadCertificate(certPath);
+        trustStore.setCertificateEntry(alias, cert);
+    }
+
+    /**
+     * Creates a truststore, adds multiple trusted certificates, and saves it to the specified path.
+     *
+     * @param trustedCertPaths List of certificate file paths to add to the truststore.
+     * @param truststorePath Path to save the generated truststore.
+     * @param truststorePassword Password for the truststore.
+     * @return Path to the saved truststore file.
+     */
+    public static Path createAndSaveTruststore(List<Path> trustedCertPaths, Path truststorePath, String truststorePassword) {
+        try {
+            KeyStore trustStore = createTruststore();
+
+            for (Path certPath : trustedCertPaths) {
+                addTrustedCertificate(trustStore, "trusted-cert-" + UUID.randomUUID(), certPath);
+            }
+
+            try (FileOutputStream fos = new FileOutputStream(truststorePath.toFile())) {
+                trustStore.store(fos, truststorePassword.toCharArray());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to save truststore to " + truststorePath + ": " + e.getMessage(), e);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create and save truststore: " + e.getMessage(), e);
+        }
+
+        return truststorePath;
+    }
+
+    public static Path createAndSaveTestTruststore(String trustStoreName, Path certificateLocations,
+            String truststorePassword) {
+        List<Path> trustedCertPaths = new ArrayList<>();
+        trustedCertPaths.add(envCa(certificateLocations).toAbsolutePath());
+        trustedCertPaths.add(envServerCert(certificateLocations).toAbsolutePath());
+
+        Path trustStorePath = testTruststorePath(trustStoreName).toAbsolutePath();
+
+        return createAndSaveTruststore(trustedCertPaths, trustStorePath, truststorePassword);
+    }
+
+}
