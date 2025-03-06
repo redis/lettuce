4c088a36789805bf21839b14f3ed98bef3ed09c8
Tihomir Krasimirov Mateev <tihomir.mateev@redis.com>
2025-03-06 16:50:59 +0200

    JSON integration tests now do not use the test-containers framework (#3203)
    * Remove testcontainers for the purpose of JSON tests
    
    * Missed three tests

--- src/test/java/io/lettuce/core/commands/CommandInterfacesIntegrationTests.java
+++ src/test/java/io/lettuce/core/commands/CommandInterfacesIntegrationTests.java
@@ -20,9 +20,7 @@
 package io.lettuce.core.commands;
 
 import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisContainerIntegrationTests;
 import io.lettuce.core.RedisURI;
-import io.lettuce.core.TestSupport;
 import io.lettuce.core.api.StatefulConnection;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
@@ -30,28 +28,17 @@ import io.lettuce.core.dynamic.Commands;
 import io.lettuce.core.dynamic.RedisCommandFactory;
 import io.lettuce.core.dynamic.annotation.Command;
 import io.lettuce.core.dynamic.annotation.Param;
-import io.lettuce.test.LettuceExtension;
-import io.lettuce.test.condition.EnabledOnCommand;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import reactor.core.publisher.Flux;
 
-import javax.inject.Inject;
 import java.lang.reflect.Proxy;
 import java.util.List;
 
 import static io.lettuce.TestTags.INTEGRATION_TEST;
-import static io.lettuce.core.SetArgs.Builder.ex;
-import static io.lettuce.core.SetArgs.Builder.exAt;
-import static io.lettuce.core.SetArgs.Builder.px;
-import static io.lettuce.core.SetArgs.Builder.pxAt;
-import static io.lettuce.core.StringMatchResult.Position;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Integration tests for {@link io.lettuce.core.dynamic.annotation.Command}.
@@ -59,7 +46,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * @author Tihomir Mateev
  */
 @Tag(INTEGRATION_TEST)
-public class CommandInterfacesIntegrationTests extends RedisContainerIntegrationTests {
+public class CommandInterfacesIntegrationTests {
 
     protected static RedisClient client;
 
