bd95177ff8f4197ad8938d0b2f476190fa4efb32
Thach Le <thach.le@linecorp.com>
2025-03-15 16:57:17 +0700

    Fix json.arrpop with root path
    Fix IntegrationTest
    
    Add Integration Test
    
    Fix test
    
    Update the logic
    
    Update logic
    
    Update the logic
    
    Correct the test
    
    Typo

--- src/main/java/io/lettuce/core/RedisJsonCommandBuilder.java
+++ src/main/java/io/lettuce/core/RedisJsonCommandBuilder.java
@@ -106,13 +106,17 @@ class RedisJsonCommandBuilder<K, V> extends BaseRedisCommandBuilder<K, V> {
 
         CommandArgs<K, V> args = new CommandArgs<>(codec).addKey(key);
 
-        if (jsonPath != null && !jsonPath.isRootPath()) {
-            // OPTIONAL as per API
-            args.add(jsonPath.toString());
-
-            if (index != -1) {
-                // OPTIONAL as per API
-                args.add(index);
+        if (jsonPath != null) {
+            if (!jsonPath.isRootPath()) {
+                args.add(jsonPath.toString());
+                if (index != -1) {
+                    args.add(index);
+                }
+            } else {
+                if (index != -1) {
+                    args.add(jsonPath.toString());
+                    args.add(index);
+                }
             }
         }
 
