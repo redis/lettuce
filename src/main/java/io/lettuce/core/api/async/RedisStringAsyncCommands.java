a2c7bbea0a0ad287b61dac45419cb130274a82c7
Igor Malinovskiy <igor.malinovskiy@redis.com>
2025-07-11 16:57:17 +0200

    Align formatting with template

--- src/main/java/io/lettuce/core/api/async/RedisStringAsyncCommands.java
+++ src/main/java/io/lettuce/core/api/async/RedisStringAsyncCommands.java
@@ -25,9 +25,9 @@ import java.util.Map;
 import io.lettuce.core.BitFieldArgs;
 import io.lettuce.core.GetExArgs;
 import io.lettuce.core.KeyValue;
+import io.lettuce.core.LcsArgs;
 import io.lettuce.core.RedisFuture;
 import io.lettuce.core.SetArgs;
-import io.lettuce.core.LcsArgs;
 import io.lettuce.core.StrAlgoArgs;
 import io.lettuce.core.StringMatchResult;
 import io.lettuce.core.output.KeyValueStreamingChannel;
@@ -349,7 +349,7 @@ public interface RedisStringAsyncCommands<K, V> {
     /**
      * Set multiple keys to multiple values.
      *
-     * @param map the map.
+     * @param map the map containing key-value pairs.
      * @return String simple-string-reply always {@code OK} since {@code MSET} can't fail.
      */
     RedisFuture<String> mset(Map<K, V> map);
@@ -357,7 +357,7 @@ public interface RedisStringAsyncCommands<K, V> {
     /**
      * Set multiple keys to multiple values, only if none of the keys exist.
      *
-     * @param map the map.
+     * @param map the map containing key-value pairs.
      * @return Boolean integer-reply specifically:
      *
      *         {@code 1} if the all the keys were set. {@code 0} if no key was set (at least one key already existed).
