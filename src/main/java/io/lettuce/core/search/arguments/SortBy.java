3e5feb066c57eecff8a452894ed392cfcc95318a
aleksandar.todorov <a_t_todorov@yahoo.com>
2025-12-01 14:01:59 +0200

    rm max, withCount from SortBy

--- src/main/java/io/lettuce/core/search/arguments/SortBy.java
+++ src/main/java/io/lettuce/core/search/arguments/SortBy.java
@@ -12,9 +12,7 @@ import io.lettuce.core.protocol.CommandKeyword;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * SORTBY post-processing operation. Sorts results by one or more properties with optional MAX optimization for top-N queries.
@@ -30,10 +28,6 @@ public class SortBy<K> implements PostProcessingOperation<K, Object> {
 
     private final List<SortProperty<K>> properties;
 
-    private Optional<Long> max = Optional.empty();
-
-    private boolean withCount = false;
-
     /**
      * Creates a new SORTBY operation.
      *
@@ -41,39 +35,6 @@ public class SortBy<K> implements PostProcessingOperation<K, Object> {
      */
     public SortBy(List<SortProperty<K>> properties) {
         this.properties = new ArrayList<>(properties);
-    }
-
-    /**
-     * Set the MAX option to optimize sorting by only processing the top N results.
-     *
-     * @param max the maximum number of results to sort
-     * @return this SortBy instance
-     */
-    public SortBy<K> max(long max) {
-        this.max = Optional.of(max);
-        return this;
-    }
-
-    /**
-     * Set the WITHCOUNT option to return accurate counts.
-     *
-     * @return this SortBy instance
-     */
-    public SortBy<K> withCount() {
-        this.withCount = true;
-        return this;
-    }
-
-    /**
-     * Static factory method to create a SortBy instance with a single property.
-     *
-     * @param property the property to sort by
-     * @param direction the sort direction
-     * @param <K> Key type
-     * @return new SortBy instance
-     */
-    public static <K> SortBy<K> of(K property, SortDirection direction) {
-        return new SortBy<>(Collections.singletonList(new SortProperty<>(property, direction)));
     }
 
     /**
@@ -102,15 +63,6 @@ public class SortBy<K> implements PostProcessingOperation<K, Object> {
                 args.add(propertyStr);
             }
             args.add(property.direction.name());
-        }
-
-        max.ifPresent(m -> {
-            args.add(CommandKeyword.MAX);
-            args.add(m);
-        });
-
-        if (withCount) {
-            args.add(CommandKeyword.WITHCOUNT);
         }
     }
 
