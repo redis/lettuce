50647eeafe676f4268543da6646eb020514cc569
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-02-12 11:46:12 +0200

    Refactor scorer

---
+++ src/main/java/io/lettuce/core/search/arguments/Scorers.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2025, Redis Ltd. and Contributors
+ * All rights reserved.
+ *
+ * Licensed under the MIT License.
+ */
+package io.lettuce.core.search.arguments;
+
+import java.util.Collections;
+import java.util.List;
+
+import io.lettuce.core.annotations.Experimental;
+
+/**
+ * Factory class for creating {@link Scorer} instances for text search.
+ *
+ * @author Aleksandar Todorov
+ * @since 7.2
+ * @see Scorer
+ */
+@Experimental
+public class Scorers {
+
+    private static Scorer simpleScorer(String name) {
+        return new Scorer(name) {
+
+            @Override
+            protected List<Object> getOwnArgs() {
+                return Collections.emptyList();
+            }
+
+        };
+    }
+
+    public static Scorer tfidf() {
+        return simpleScorer("TFIDF");
+    }
+
+    public static Scorer tfidfDocNorm() {
+        return simpleScorer("TFIDF.DOCNORM");
+    }
+
+    public static Scorer bm25() {
+        return simpleScorer("BM25STD");
+    }
+
+    public static Scorer bm25Norm() {
+        return simpleScorer("BM25STD_NORM");
+    }
+
+    public static Scorer dismax() {
+        return simpleScorer("DISMAX");
+    }
+
+    public static Scorer docscore() {
+        return simpleScorer("DOCSCORE");
+    }
+
+    public static Scorer hamming() {
+        return simpleScorer("HAMMING");
+    }
+
+}
