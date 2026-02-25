ac3edd60ece4d7f71064b3e12ae1f4f8bfa65cdb
Thach Le <thach.le@linecorp.com>
2026-02-25 14:48:49 +0700

    Deprecate setex

--- src/main/java/io/lettuce/core/AbstractRedisAsyncCommands.java
+++ src/main/java/io/lettuce/core/AbstractRedisAsyncCommands.java
@@ -2781,6 +2781,7 @@ public abstract class AbstractRedisAsyncCommands<K, V> implements RedisAclAsyncC
         return dispatch(commandBuilder.setbit(key, offset, value));
     }
 
+    @Deprecated
     @Override
     public RedisFuture<String> setex(K key, long seconds, V value) {
         return dispatch(commandBuilder.setex(key, seconds, value));
--- src/main/java/io/lettuce/core/AbstractRedisReactiveCommands.java
+++ src/main/java/io/lettuce/core/AbstractRedisReactiveCommands.java
@@ -2845,6 +2845,7 @@ public abstract class AbstractRedisReactiveCommands<K, V>
         return createMono(() -> commandBuilder.setbit(key, offset, value));
     }
 
+    @Deprecated
     @Override
     public Mono<String> setex(K key, long seconds, V value) {
         return createMono(() -> commandBuilder.setex(key, seconds, value));
