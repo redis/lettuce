2486467a01d8ae2b2b60094c874ed1d96f85216a
yordantsintsov <yordan.tsintsov@gmail.com>
2026-06-02 15:19:08 +0300

    Added cluster tests.
    Signed-off-by: yordantsintsov <yordan.tsintsov@gmail.com>

---
+++ src/test/java/io/lettuce/core/bf/RedisBloomFilterClusterIntegrationTests.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2026, Redis Ltd. and Contributors
+ * All rights reserved.
+ *
+ * Licensed under the MIT License.
+ */
+package io.lettuce.core.bf;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Tag;
+
+import io.lettuce.core.cluster.ClusterTestUtil;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+
+import static io.lettuce.TestTags.INTEGRATION_TEST;
+
+/**
+ * Integration tests for Redis Array commands using Redis Cluster.
+ *
+ * @author Yordan Tsintsov
+ * @since 7.7
+ */
+@Tag(INTEGRATION_TEST)
+public class RedisBloomFilterClusterIntegrationTests extends RedisBloomFilterIntegrationTests {
+
+    @Inject
+    RedisBloomFilterClusterIntegrationTests(StatefulRedisClusterConnection<String, String> connection) {
+        super(ClusterTestUtil.redisCommandsOverCluster(connection));
+    }
+
+}
