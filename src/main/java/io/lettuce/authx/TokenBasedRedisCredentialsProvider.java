30282242e6b57c649126218c8f48688ea1e284da
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-07-03 10:40:19 +0300

    Revert "- improve exceptions due to  executor initialization"
    This reverts commit db59e56cace46012e6877623e7178c0d2492c29a.

--- src/main/java/io/lettuce/authx/TokenBasedRedisCredentialsProvider.java
+++ src/main/java/io/lettuce/authx/TokenBasedRedisCredentialsProvider.java
@@ -126,13 +126,13 @@ public class TokenBasedRedisCredentialsProvider implements RedisCredentialsProvi
 
     @Override
     public void accept(ClientResources clientResources) {
-        if (executor == null) {
+        if (this.executor == null) {
             executor = clientResources.eventExecutorGroup().next();
         }
     }
 
     private void init() {
-        LettuceAssert.notNull(executor, "Executor must be provided before initialization");
+
         TokenListener listener = new TokenListener() {
 
             @Override
@@ -244,7 +244,6 @@ public class TokenBasedRedisCredentialsProvider implements RedisCredentialsProvi
             subscription.close();
             throw new IllegalStateException("Credentials provider closed");
         }
-        LettuceAssert.notNull(executor, "Executor must be provided before subscription");
         executor.execute(() -> subscription.replay(getReplayCandidate()));
         return subscription;
     }
@@ -344,10 +343,7 @@ public class TokenBasedRedisCredentialsProvider implements RedisCredentialsProvi
      * @return a started {@link TokenBasedRedisCredentialsProvider}
      */
     public static TokenBasedRedisCredentialsProvider create(TokenManager tokenManager) {
-        LettuceAssert.notNull(tokenManager, "TokenManager must not be null");
-        TokenBasedRedisCredentialsProvider credentialManager = new TokenBasedRedisCredentialsProvider(tokenManager, null);
-        credentialManager.init();
-        return credentialManager;
+        return create(tokenManager, null);
     }
 
     /**
