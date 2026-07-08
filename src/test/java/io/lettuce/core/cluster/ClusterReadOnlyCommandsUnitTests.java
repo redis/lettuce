53c5f90f2101032fc111827f2564b009c6787bea
njm1250 <njm1250@gmail.com>
2026-07-08 18:53:02 +0900

    Add Vector Set commands to read-only command list #3824

--- src/test/java/io/lettuce/core/cluster/ClusterReadOnlyCommandsUnitTests.java
+++ src/test/java/io/lettuce/core/cluster/ClusterReadOnlyCommandsUnitTests.java
@@ -20,7 +20,7 @@ class ClusterReadOnlyCommandsUnitTests {
 
     @Test
     void testCount() {
-        assertThat(ClusterReadOnlyCommands.getReadOnlyCommands()).hasSize(127);
+        assertThat(ClusterReadOnlyCommands.getReadOnlyCommands()).hasSize(135);
     }
 
     @Test
