b8b2a25e888d60b01f1aaf4e5347c359c01c256c
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-07-13 09:47:58 +0300

    Add static methods to new CredentialsProvider

--- src/main/java/io/lettuce/core/CredentialsProvider.java
+++ src/main/java/io/lettuce/core/CredentialsProvider.java
@@ -4,8 +4,12 @@
  */
 package io.lettuce.core;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.function.Supplier;
 
+import io.lettuce.core.internal.Futures;
+import io.lettuce.core.internal.LettuceAssert;
 import reactor.core.publisher.Mono;
 
 /**
@@ -43,6 +47,61 @@ public interface CredentialsProvider extends RedisCredentialsProvider {
     @Override
     default Mono<RedisCredentials> resolveCredentials() {
         return Mono.fromCompletionStage(resolveCredentialsAsync());
+    }
+
+    /**
+     * Creates a new {@link CredentialsProvider} from a given {@link Supplier}.
+     *
+     * @param supplier must not be {@code null}.
+     * @return a {@link CredentialsProvider} resolving the {@link RedisCredentials} produced by the {@link Supplier}.
+     */
+    static CredentialsProvider from(Supplier<RedisCredentials> supplier) {
+
+        LettuceAssert.notNull(supplier, "Supplier must not be null");
+
+        return () -> {
+            try {
+                RedisCredentials credentials = supplier.get();
+                if (credentials == null) {
+                    return Futures.failed(new IllegalStateException("Provided RedisCredentials supplier returned null"));
+                }
+                return CompletableFuture.completedFuture(credentials);
+            } catch (Exception e) {
+                return Futures.failed(e);
+            }
+        };
+    }
+
+    /**
+     * Extension to {@link CredentialsProvider} that resolves credentials immediately without the need to defer the credential
+     * resolution.
+     */
+    @FunctionalInterface
+    interface ImmediateRedisCredentialsProvider extends CredentialsProvider {
+
+        @Override
+        default CompletionStage<RedisCredentials> resolveCredentialsAsync() {
+            try {
+                RedisCredentials credentials = resolveCredentialsNow();
+                if (credentials == null) {
+                    return Futures.failed(new IllegalStateException("RedisCredentials resolved to null"));
+                }
+                return CompletableFuture.completedFuture(credentials);
+            } catch (Exception e) {
+                return Futures.failed(e);
+            }
+        }
+
+        /**
+         * Returns {@link RedisCredentials} that can be used to authorize a Redis connection. Each implementation of
+         * {@code CredentialsProvider} can choose its own strategy for loading credentials. For example, an implementation might
+         * load credentials from an existing key management system, or load new credentials when credentials are rotated. If an
+         * error occurs during the loading of credentials or credentials could not be found, a runtime exception will be raised.
+         *
+         * @return the resolved {@link RedisCredentials} that can be used to authorize a Redis connection.
+         */
+        RedisCredentials resolveCredentialsNow();
+
     }
 
 }
