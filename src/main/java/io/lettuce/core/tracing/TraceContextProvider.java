8139ddd5fa61c04a9cfe0f3b68f5191d20495f2a
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-08-14 16:51:22 +0300

    Revert "[reactor-optional] Deprecate reactive in TraceContextProvider (#3827)"
    This reverts commit cd2d96f547af45d556d7ce86692eaee474e521d4.

--- src/main/java/io/lettuce/core/tracing/TraceContextProvider.java
+++ src/main/java/io/lettuce/core/tracing/TraceContextProvider.java
@@ -1,10 +1,5 @@
 package io.lettuce.core.tracing;
 
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
-
 import reactor.core.publisher.Mono;
 
 /**
@@ -17,57 +12,15 @@ import reactor.core.publisher.Mono;
 public interface TraceContextProvider {
 
     /**
-     * Returns the {@link TraceContext} in a blocking fashion.
-     * <p>
-     * Return value can be null depending on the implementation, and application context it is called from.
-     * 
      * @return the {@link TraceContext}.
      */
     TraceContext getTraceContext();
 
     /**
-     * Returns the {@link TraceContext} in a deferred fashion as a {@link Mono}.
-     * <p>
-     * The emitted value may be {@code null} depending on the implementation and the application context it is called from.
-     *
-     * @return a {@link Mono} emitting the {@link TraceContext}.
-     * @deprecated since 7.7, override {@link #getTraceContextAsync(Map)} instead; scheduled for removal in Lettuce 8.0.
+     * @return the {@link TraceContext}.
      */
-    @Deprecated
     default Mono<TraceContext> getTraceContextLater() {
-        return Mono.deferContextual(ctx -> Mono.justOrEmpty(getTraceContextAsync(new AbstractMap<Object, Object>() {
-
-            @Override
-            public Object get(Object key) {
-                return ctx.hasKey(key) ? ctx.get(key) : null;
-            }
-
-            @Override
-            public boolean containsKey(Object key) {
-                return ctx.hasKey(key);
-            }
-
-            @Override
-            public Set<Entry<Object, Object>> entrySet() {
-                throw new UnsupportedOperationException();
-            }
-
-        }).get()));
-    }
-
-    /**
-     * Returns a {@link Supplier} that resolves the {@link TraceContext} on demand, using the given application context to
-     * obtain or populate a particular context where required.
-     * <p>
-     * The value produced by the {@link Supplier} may be {@code null} depending on the implementation and the application
-     * context it is called from.
-     *
-     * @param appContext the application context used to resolve the {@link TraceContext}.
-     * @return a {@link Supplier} of the {@link TraceContext}.
-     * @since 7.7
-     */
-    default Supplier<TraceContext> getTraceContextAsync(Map<Object, Object> appContext) {
-        return this::getTraceContext;
+        return Mono.justOrEmpty(getTraceContext());
     }
 
 }
