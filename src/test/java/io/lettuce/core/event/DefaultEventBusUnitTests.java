44f3e8f5182dd1d0ae2f870147273823c9ae16d0
aleksandar.todorov <a_t_todorov@yahoo.com>
2026-09-15 15:58:24 +0300

    [reactor-optional] Deprecate reactor based EventBus in favor of callback based Subscription (#3789)

--- src/test/java/io/lettuce/core/event/DefaultEventBusUnitTests.java
+++ src/test/java/io/lettuce/core/event/DefaultEventBusUnitTests.java
@@ -4,6 +4,8 @@ import static io.lettuce.TestTags.UNIT_TEST;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import io.lettuce.core.Subscription;
 import reactor.core.Disposable;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
@@ -47,6 +50,76 @@ class DefaultEventBusUnitTests {
         assertThat(arrayQueue.take()).isEqualTo(event);
         assertThat(arrayQueue.take()).isEqualTo(event);
         disposable1.dispose();
+    }
+
+    @Test
+    void subscribeCallbackReceivesEvent() throws Exception {
+
+        EventBus sut = new DefaultEventBus(Schedulers.immediate());
+        ArrayBlockingQueue<Event> received = new ArrayBlockingQueue<>(5);
+
+        Subscription subscription = sut.subscribe(received::add);
+        sut.publish(event);
+
+        assertThat(received.poll(1, TimeUnit.SECONDS)).isEqualTo(event);
+        subscription.close();
+    }
+
+    @Test
+    void subscribeByTypeFiltersEvents() throws Exception {
+
+        EventBus sut = new DefaultEventBus(Schedulers.immediate());
+        ArrayBlockingQueue<EventA> received = new ArrayBlockingQueue<>(5);
+
+        Subscription subscription = sut.subscribe(EventA.class, received::add);
+        sut.publish(new EventB());
+        EventA expected = new EventA();
+        sut.publish(expected);
+
+        assertThat(received.poll(1, TimeUnit.SECONDS)).isSameAs(expected);
+        assertThat(received).isEmpty();
+        subscription.close();
+    }
+
+    @Test
+    void closeStopsDelivery() throws Exception {
+
+        EventBus sut = new DefaultEventBus(Schedulers.immediate());
+        ArrayBlockingQueue<Event> received = new ArrayBlockingQueue<>(5);
+
+        Subscription subscription = sut.subscribe(received::add);
+        subscription.close();
+        sut.publish(event);
+
+        assertThat(received.poll(200, TimeUnit.MILLISECONDS)).isNull();
+    }
+
+    @Test
+    void listenerExceptionDoesNotCancelSubscription() throws Exception {
+
+        EventBus sut = new DefaultEventBus(Schedulers.immediate());
+        ArrayBlockingQueue<Event> received = new ArrayBlockingQueue<>(5);
+        AtomicBoolean firstEvent = new AtomicBoolean(true);
+
+        Subscription subscription = sut.subscribe(e -> {
+            if (firstEvent.compareAndSet(true, false)) {
+                throw new IllegalStateException("listener failure");
+            }
+            received.add(e);
+        });
+
+        sut.publish(new EventA()); // listener throws on this one, subscription must survive
+        EventB expected = new EventB();
+        sut.publish(expected); // must still be delivered
+
+        assertThat(received.poll(1, TimeUnit.SECONDS)).isSameAs(expected);
+        subscription.close();
+    }
+
+    static class EventA implements Event {
+    }
+
+    static class EventB implements Event {
     }
 
 }
